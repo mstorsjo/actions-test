9524dd93a387fc0c23fe6320454f407b98c780ab
Martin Storsjö <martin@martin.st>
2022-06-11 00:57:05 +0300

    WIP: Update run-tests.sh

--- llvm-mingw/run-tests.sh
+++ llvm-mingw/run-tests.sh
@@ -49,13 +49,15 @@ if [ -z "$RUN_X86" ]; then
     MINGW*|MSYS*)
         NATIVE_X86=1
         # A non-empty string to trigger running, even if no wrapper is needed.
-        RUN_X86=" "
+        RUN_X86_64=" "
+        RUN_I686=" "
         export PATH=.:$PATH
         ;;
     *)
         case $(uname -m) in
         x86_64)
-            RUN_X86=wine
+            : ${RUN_X86_64:=wine}
+            : ${RUN_I686:=wine}
             ;;
         esac
         ;;
@@ -83,9 +85,14 @@ TESTS_IDL="idltest"
 TESTS_OTHER_TARGETS="hello"
 for arch in $ARCHS; do
     case $arch in
-    i686|x86_64)
-        RUN="$RUN_X86"
-        COPY="$COPY_X86"
+    i686)
+        RUN="$RUN_I686"
+        COPY="$COPY_I686"
+        NATIVE="$NATIVE_X86"
+        ;;
+    x86_64)
+        RUN="$RUN_X86_64"
+        COPY="$COPY_X86_64"
         NATIVE="$NATIVE_X86"
         ;;
     armv7)
@@ -249,7 +256,7 @@ for arch in $ARCHS; do
     fi
     for test in $RUN_TESTS; do
         file=$test.exe
-        if [ -n "$RUN" ]; then
+        if [ -n "$RUN" ] && [ "$RUN" != "false" ]; then
             $RUN $file
         fi
     done
