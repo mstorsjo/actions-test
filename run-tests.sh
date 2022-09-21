998d654f118ddaa0ece7a2049277bead483df269
Martin Storsjö <martin@martin.st>
2022-09-21 15:20:05 +0300

    WIP: run-tests: Do check for the specific output from ssp functions

--- run-tests.sh
+++ run-tests.sh
@@ -302,14 +302,10 @@ for arch in $ARCHS; do
                         grep -q "signed integer overflow" $OUT
                         ;;
                     stacksmash)
-                        # GNU libssp writes this directly to the console,
-                        # and it can't be redirected, so we can't check for its presence.
-                        #grep -q "stack smashing detected" $OUT
+                        grep -q "stack smashing detected" $OUT
                         ;;
                     bufferoverflow-*)
-                        # GNU libssp writes this directly to the console,
-                        # and it can't be redirected, so we can't check for its presence.
-                        #grep -q "buffer overflow detected" $OUT
+                        grep -q "buffer overflow detected" $OUT
                         ;;
                     *)
                         echo Unhandled failure test $test
