92bf969f8647e90ecb5bea770052703c388334bc
Martin Storsjö <martin@martin.st>
2023-04-19 10:15:25 +0300

    Sync to the latest upstream llvm-mingw

---
+++ build-cross-tools.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+#
+# Copyright (c) 2018 Martin Storsjo
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+set -e
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    --with-python)
+        PYTHON=1
+        ;;
+    --disable-lldb)
+        LLVM_ARGS="$LLVM_ARGS $1"
+        NO_LLDB=1
+        ;;
+    --disable-lldb-mi)
+        NO_LLDB_MI=1
+        ;;
+    --disable-clang-tools-extra)
+        LLVM_ARGS="$LLVM_ARGS $1"
+        ;;
+    --disable-mingw-w64-tools)
+        NO_MINGW_W64_TOOLS=1
+        ;;
+    --disable-make)
+        NO_MAKE=1
+        ;;
+    *)
+        if [ -z "$NATIVE" ]; then
+            NATIVE="$1"
+        elif [ -z "$PREFIX" ]; then
+            PREFIX="$1"
+        elif [ -z "$CROSS_ARCH" ]; then
+            CROSS_ARCH="$1"
+        else
+            echo Unrecognized parameter $1
+            exit 1
+        fi
+        ;;
+    esac
+    shift
+done
+if [ -z "$CROSS_ARCH" ]; then
+    echo $0 native prefix arch [--with-python] [--disable-lldb] [--disable-lldb-mi] [--disable-clang-tool-extra] [--disable-mingw-w64-tools] [--disable-make]
+    exit 1
+fi
+
+for dep in git curl cmake; do
+    if ! command -v $dep >/dev/null; then
+        echo "$dep not installed. Please install it and retry" 1>&2
+        exit 1
+    fi
+done
+
+export PATH="$NATIVE/bin:$PATH"
+HOST=$CROSS_ARCH-w64-mingw32
+
+if [ -n "$PYTHON" ]; then
+    PYTHON_NATIVE_PREFIX="$(cd "$(dirname "$0")" && pwd)/python-native"
+    [ -d "$PYTHON_NATIVE_PREFIX" ] || rm -rf "$PYTHON_NATIVE_PREFIX"
+    ./build-python.sh $PYTHON_NATIVE_PREFIX
+    export PATH="$PYTHON_NATIVE_PREFIX/bin:$PATH"
+    ./build-python.sh $PREFIX/python --host=$HOST
+    mkdir -p $PREFIX/bin
+    cp $PREFIX/python/bin/*.dll $PREFIX/bin
+    LLVM_ARGS="$LLVM_ARGS --with-python"
+fi
+
+./build-llvm.sh $PREFIX --host=$HOST $LLVM_ARGS
+if [ -z "$NO_LLDB" ] && [ -z "$NO_LLDB_MI" ]; then
+    ./build-lldb-mi.sh $PREFIX --host=$HOST
+fi
+./strip-llvm.sh $PREFIX --host=$HOST
+if [ -z "$NO_MINGW_W64_TOOLS" ]; then
+    ./build-mingw-w64-tools.sh $PREFIX --skip-include-triplet-prefix --host=$HOST
+fi
+./install-wrappers.sh $PREFIX --host=$HOST
+./prepare-cross-toolchain.sh $NATIVE $PREFIX $CROSS_ARCH
+if [ -z "$NO_MAKE" ]; then
+    ./build-make.sh $PREFIX --host=$HOST
+fi
---
+++ build-make.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+#
+# Copyright (c) 2018 Martin Storsjo
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+set -e
+
+unset HOST
+
+: ${MAKE_VERSION:=4.2.1}
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    --host=*)
+        HOST="${1#*=}"
+        ;;
+    *)
+        PREFIX="$1"
+        ;;
+    esac
+    shift
+done
+if [ -z "$PREFIX" ]; then
+    echo $0 [--host=triple] dest
+    exit 1
+fi
+
+mkdir -p "$PREFIX"
+PREFIX="$(cd "$PREFIX" && pwd)"
+
+: ${CORES:=$(nproc 2>/dev/null)}
+: ${CORES:=$(sysctl -n hw.ncpu 2>/dev/null)}
+: ${CORES:=4}
+: ${ARCHS:=${TOOLCHAIN_ARCHS-i686 x86_64 armv7 aarch64}}
+
+download() {
+    if command -v wget >/dev/null; then
+        wget "$1"
+    else
+        curl -LO "$1"
+    fi
+}
+
+if [ ! -d make-$MAKE_VERSION ]; then
+    download https://ftp.gnu.org/gnu/make/make-$MAKE_VERSION.tar.bz2
+    tar -jxf make-$MAKE_VERSION.tar.bz2
+fi
+
+cd make-$MAKE_VERSION
+
+if [ -n "$HOST" ]; then
+    CONFIGFLAGS="$CONFIGFLAGS --host=$HOST"
+    CROSS_NAME=-$HOST
+fi
+
+mkdir -p build$CROSS_NAME
+cd build$CROSS_NAME
+../configure --prefix="$PREFIX" $CONFIGFLAGS --program-prefix=mingw32- --enable-job-server LDFLAGS="-Wl,-s"
+make -j$CORES
+make install-binPROGRAMS
