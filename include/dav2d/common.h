74d46eaa22d7f27705c90806bfe251dcdc83a7e7
KO Myung-Hun <komh@chollian.net>
2023-07-03 23:20:03 +0900

    Export APIs correctly on OS/2

--- include/dav2d/common.h
+++ include/dav2d/common.h
@@ -43,6 +43,8 @@ extern "C" {
       #else
         #define DAV2D_API
       #endif
+    #elif defined __OS2__
+        #define DAV2D_API __declspec(dllexport)
     #else
       #if __GNUC__ >= 4
         #define DAV2D_API __attribute__ ((visibility ("default")))
