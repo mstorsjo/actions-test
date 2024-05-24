87a8a45135b6fa78ea5239e0bacec2ec20b2d096
Martin Storsjö <martin@martin.st>
2024-05-24 11:00:14 +0300

    Remove printing of the input command line length

--- clang-target-wrapper.c
+++ clang-target-wrapper.c
@@ -30,9 +30,6 @@ int _tmain(int argc, TCHAR* argv[]) {
     const TCHAR *target;
     const TCHAR *exe;
 
-    LPWSTR cmdline = GetCommandLineW();
-    printf("input cmdline %d chars\n", (int)wcslen(cmdline));
-
     split_argv(argv[0], &dir, NULL, &target, &exe);
     if (!target)
         target = _T(DEFAULT_TARGET);
