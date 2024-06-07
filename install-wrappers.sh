169d5709a227f3510be369b1e6a24b00d7c5d3e3
Martin Storsjö <martin@martin.st>
2024-06-07 14:35:43 +0300

    Update wrappers to the latest version

--- install-wrappers.sh
+++ install-wrappers.sh
@@ -17,9 +17,15 @@
 set -e
 
 unset HOST
+unset HOST_CLANG
 
 while [ $# -gt 0 ]; do
     case "$1" in
+    --host-clang|--host-clang=*)
+        HOST_CLANG=${1#--host-clang}
+        HOST_CLANG=${HOST_CLANG#=}
+        HOST_CLANG=${HOST_CLANG:-clang}
+        ;;
     --host=*)
         HOST="${1#*=}"
         ;;
@@ -30,7 +36,7 @@ while [ $# -gt 0 ]; do
     shift
 done
 if [ -z "$PREFIX" ]; then
-    echo $0 [--host=triple] dest
+    echo $0 [--host=triple] [--host-clang[=clang]] dest
     exit 1
 fi
 mkdir -p "$PREFIX"
@@ -58,6 +64,44 @@ else
     esac
 fi
 
+if [ -n "${HOST_CLANG}" ]; then
+    HOST_CLANG_EXE=$(command -v $HOST_CLANG)
+    HOST_CLANG_VER=$(echo "__clang_major__ __clang_minor__ __clang_patchlevel__" | $HOST_CLANG_EXE -E -P -x c - | xargs printf '%d.%d.%d')
+
+    mkdir -p $PREFIX/bin
+
+    # ex. /usr/lib/llvm-17/lib/clang/17
+    resdir=$($HOST_CLANG -print-resource-dir)
+    # ex. /usr/lib/llvm-17
+    llvmdir=${resdir%/lib/clang/*}
+    # ex /lib/clang/17
+    clangres=${resdir#$llvmdir}
+
+    mkdir -p $PREFIX$clangres
+
+    # link the header directory, prevent modification
+    ln -snf $resdir/include $PREFIX$clangres/include
+
+    # Note: clang will detect the "InstalledDir" based on the path that was used to invoke the tools
+    # This might still have some hidden effects
+    printf '#!/bin/sh\nsr=$(dirname "$(dirname "$(readlink -f "$0")")")\nexec %s -resource-dir="$sr"%s --sysroot="$sr" "$@"\n' "$HOST_CLANG_EXE" "$clangres" > $PREFIX/bin/clang
+    # printf '#!/bin/sh\nsr=$(dirname "$(dirname "$(readlink -f "$0")")")\nexec %s -resource-dir="$sr"%s --sysroot="$sr" "$@"\n' "$(readlink -f "$HOST_CLANG_EXE")" "$clangres" > $PREFIX/bin/clang
+    chmod 755 $PREFIX/bin/clang
+    ln -sf clang $PREFIX/bin/clang++
+    ln -sf clang $PREFIX/bin/clang-cpp
+
+    echo "Using existing clang $HOST_CLANG_EXE ($HOST_CLANG_VER)"
+    $PREFIX/bin/clang -v
+
+    # prefer system llvm installation, but search in llvm private paths (eg. debian does not symlink all tools into /usr/bin)
+    llvmexec="$PATH:$llvmdir/bin"
+
+    for exec in ld.lld llvm-ar llvm-ranlib llvm-nm llvm-objcopy llvm-strip llvm-rc llvm-cvtres \
+                llvm-addr2line llvm-dlltool llvm-readelf llvm-size llvm-strings llvm-addr2line llvm-windres llvm-ml; do
+        execpath=$(PATH=$llvmexec command -v $exec) && ln -sf $execpath $PREFIX/bin/$exec
+    done
+fi
+
 if [ -n "$MACOS_REDIST" ]; then
     : ${MACOS_REDIST_ARCHS:=arm64 x86_64}
     : ${MACOS_REDIST_VERSION:=10.9}
@@ -70,6 +114,11 @@ fi
 if [ -n "$EXEEXT" ]; then
     CLANG_MAJOR=$(basename $(echo $PREFIX/lib/clang/* | awk '{print $NF}') | cut -f 1 -d .)
     WRAPPER_FLAGS="$WRAPPER_FLAGS -municode -DCLANG=\"clang-$CLANG_MAJOR\""
+    WRAPPER_FLAGS="$WRAPPER_FLAGS -DCLANG_SCAN_DEPS=\"clang-scan-deps-real\""
+    # The wrappers may use printf, but doesn't use anything that specifically
+    # needs full ansi compliance - prefer leaner binaries by using the CRT
+    # implementations.
+    WRAPPER_FLAGS="$WRAPPER_FLAGS -D__USE_MINGW_ANSI_STDIO=0"
 fi
 
 mkdir -p "$PREFIX/bin"
@@ -82,6 +131,7 @@ if [ -n "$HOST" ] && [ -n "$EXEEXT" ]; then
     done
 fi
 $CC wrappers/clang-target-wrapper.c -o "$PREFIX/bin/clang-target-wrapper$EXEEXT" -O2 -Wl,-s $WRAPPER_FLAGS
+$CC wrappers/clang-scan-deps-wrapper.c -o "$PREFIX/bin/clang-scan-deps-wrapper$EXEEXT" -O2 -Wl,-s $WRAPPER_FLAGS
 $CC wrappers/llvm-wrapper.c -o "$PREFIX/bin/llvm-wrapper$EXEEXT" -O2 -Wl,-s $WRAPPER_FLAGS
 if [ -n "$EXEEXT" ]; then
     # For Windows, we should prefer the executable wrapper, which also works
@@ -97,6 +147,7 @@ for arch in $ARCHS; do
         for exec in clang clang++ gcc g++ c++ as; do
             ln -sf clang-target-wrapper$CTW_SUFFIX $arch-w64-$target_os-$exec$CTW_LINK_SUFFIX
         done
+        ln -sf clang-scan-deps-wrapper$CTW_SUFFIX $arch-w64-$target_os-clang-scan-deps$CTW_LINK_SUFFIX
         for exec in addr2line ar ranlib nm objcopy readelf size strings strip llvm-ar llvm-ranlib; do
             if [ -n "$EXEEXT" ]; then
                 link_target=llvm-wrapper
@@ -125,6 +176,9 @@ if [ -n "$EXEEXT" ]; then
     if [ ! -L clang$EXEEXT ] && [ -f clang$EXEEXT ] && [ ! -f clang-$CLANG_MAJOR$EXEEXT ]; then
         mv clang$EXEEXT clang-$CLANG_MAJOR$EXEEXT
     fi
+    if [ ! -L clang-scan-deps$EXEEXT ] && [ -f clang-scan-deps$EXEEXT ] && [ ! -f clang-scan-deps-real$EXEEXT ]; then
+        mv clang-scan-deps$EXEEXT clang-scan-deps-real$EXEEXT
+    fi
     if [ -z "$HOST" ]; then
         HOST=$(./clang-$CLANG_MAJOR -dumpmachine | sed 's/-.*//')-w64-mingw32
     fi
@@ -133,7 +187,7 @@ if [ -n "$EXEEXT" ]; then
     # we are installing wrappers for.
     case $ARCHS in
     *$HOST_ARCH*)
-        for exec in clang clang++ gcc g++ c++ addr2line ar dlltool ranlib nm objcopy readelf size strings strip windres; do
+        for exec in clang clang++ gcc g++ c++ addr2line ar dlltool ranlib nm objcopy readelf size strings strip windres clang-scan-deps; do
             ln -sf $HOST-$exec$EXEEXT $exec$EXEEXT
         done
         for exec in cc c99 c11; do
