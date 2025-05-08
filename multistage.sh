0b32547fa75bbf9963a745daa862cdf37792759c
Martin Storsjö <martin@martin.st>
2025-05-08 16:01:40 +0300

    WIP: Add a dockerfile for building a first stage build of only LLVM

---
+++ multistage.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Copyright (c) 2025 Martin Storsjo
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+set -ex
+
+time docker build -f Dockerfile.stage1 . -t llvm-mingw:stage1
+
+docker run --rm llvm-mingw:stage1 sh -c "cd /opt && tar -Jcvf - --format=ustar --numeric-owner --owner=0 --group=0 llvm" > llvm-stage1.tar.xz
+
