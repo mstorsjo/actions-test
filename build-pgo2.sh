0e41f89fa6d4e9453acd61e0b584b894d58ce180
Martin Storsjö <martin@martin.st>
2025-05-12 00:21:32 +0300

    Add a new script for doing the PGO build

---
+++ build-pgo2.sh
@@ -0,0 +1,15 @@
+#1/bin/sh
+set -ex
+#./build-llvm-local.sh $HOME/clang-stage1
+rm -rf $HOME/clang-stage1
+./build-all.sh $HOME/clang-stage1 --disable-lldb --disable-clang-tools-extra
+time ./build-compiler-rt.sh --native $HOME/clang-stage1
+rm -rf $HOME/clang-profile
+time cp -a $HOME/clang-stage1 $HOME/clang-profile
+time (export PATH=$HOME/clang-stage1/bin:$PATH; LLVM_PROFILE_DATA_DIR=/tmp/llvm-profile CLEAN=1 ./build-llvm.sh $HOME/clang-profile --disable-lldb --disable-clang-tools-extra --stage2 --disable-dylib --instrumented=Frontend)
+rm -rf profile.profdata
+time ./train-profile.sh $HOME/clang-profile
+rm -rf $HOME/clang-pgo
+time cp -a $HOME/clang-stage1 $HOME/clang-pgo
+time (export PATH=$HOME/clang-stage1/bin:$PATH; LLVM_PROFDATA_FILE=$(pwd)/profile.profdata CLEAN=1 ./build-llvm.sh $HOME/clang-pgo --stage2 --thinlto)
+
