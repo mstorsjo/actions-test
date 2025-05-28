5b3c8c180368c6dc701859c8ae392a9cfc2ed21a
Martin Storsjö <martin@martin.st>
2025-05-29 00:18:55 +0300

    WIP: build-pgo2: Try to update this script

--- build-pgo2.sh
+++ build-pgo2.sh
@@ -5,12 +5,32 @@ COMPILER_LAUNCHER=ccache LLVM_CMAKEFLAGS="-DLLVM_USE_RELATIVE_PATHS_IN_FILES=ON"
 time ./build-compiler-rt.sh --native $HOME/clang-stage1
 
 rm -rf $HOME/clang-profile
-time (export PATH=$HOME/clang-stage1/bin:$PATH; LLVM_PROFILE_DATA_DIR=/tmp/llvm-profile CLEAN=1 ./build-llvm.sh $HOME/clang-profile --disable-lldb --disable-clang-tools-extra --stage2 --disable-dylib --instrumented=Frontend)
+time (export PATH=$HOME/clang-stage1/bin:$PATH; CLEAN=1 ./build-llvm.sh $HOME/clang-profile --disable-lldb --disable-clang-tools-extra --with-clang --disable-dylib --instrumented=Frontend)
 
-rm -rf profile.profdata
-time ./pgo-training.sh $HOME/clang-profile
+time ./pgo-training.sh $HOME/clang-profile $HOME/clang-stage1
 
 rm -rf $HOME/clang-pgo
 time cp -a $HOME/clang-stage1 $HOME/clang-pgo
-time (export PATH=$HOME/clang-stage1/bin:$PATH; LLVM_PROFDATA_FILE=$(pwd)/profile.profdata CLEAN=1 ./build-llvm.sh $HOME/clang-pgo --stage2 --thinlto)
+time (export PATH=$HOME/clang-stage1/bin:$PATH; CLEAN=1 ./build-llvm.sh $HOME/clang-pgo --with-clang --thinlto --profile=profile.profdata; ./build-lldb-mi.sh $HOME/clang-pgo; ./strip-llvm.sh $HOME/clang-pgo)
+
+
+
+# ./build-all.sh --stage1 $HOME/clang-stage1
+#     --disable-lldb --disable-clang-tools-extra
+
+# ./build-all.sh --profile $HOME/clang-stage1 $HOME/clang-profile
+#   build-compiler-rt.sh --native
+#   build-llvm (BUILDDIR=...)
+#   pgo-training.sh
+
+# ./build-all.sh --pgo $HOME/clang-stage1 $HOME/clang-pgo
+#  cp -a stage1
+#  --llvm-only
+
+# ./build-all.sh --full-pgo $HOME/clang-stage1 $HOME/clang-pgo
+# --stage1, --profile, --full-pgo
+# CLEAN=1 for later stages
+# unset COMPILER_LAUNCHER after first step
+
+# fold -DLLVM_USE_RELATIVE_PATHS_IN_FILES into build-llvm.sh
 
