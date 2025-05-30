391cd90fc877be49e5ada0bb34413be8c84c5a39
Martin Storsjö <martin@martin.st>
2025-05-31 00:31:17 +0300

    build-pgo2: Update for not installing the instrumented build

--- build-pgo2.sh
+++ build-pgo2.sh
@@ -4,10 +4,9 @@ rm -rf $HOME/clang-stage1
 COMPILER_LAUNCHER=ccache LLVM_CMAKEFLAGS="-DLLVM_USE_RELATIVE_PATHS_IN_FILES=ON" ./build-all.sh $HOME/clang-stage1 --disable-lldb --disable-clang-tools-extra
 time ./build-compiler-rt.sh --native $HOME/clang-stage1
 
-rm -rf $HOME/clang-profile
-time (export PATH=$HOME/clang-stage1/bin:$PATH; CLEAN=1 ./build-llvm.sh $HOME/clang-profile --disable-lldb --disable-clang-tools-extra --with-clang --disable-dylib --instrumented=Frontend)
+time (export PATH=$HOME/clang-stage1/bin:$PATH; CLEAN=1 ./build-llvm.sh /tmp/clang-profile-dummy --disable-lldb --disable-clang-tools-extra --with-clang --disable-dylib --instrumented=Frontend)
 
-time ./pgo-training.sh $HOME/clang-profile $HOME/clang-stage1
+time ./pgo-training.sh llvm-project/llvm/build-instrumented $HOME/clang-stage1
 
 rm -rf $HOME/clang-pgo
 time cp -a $HOME/clang-stage1 $HOME/clang-pgo
