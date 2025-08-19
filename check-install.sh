11ca3f04fa5739700bc7bc74acb78bcd20508a47
Martin Storsjo <martin@martin.st>
2025-08-19 13:56:50 +0300

    check-install: Test meson too

--- check-install.sh
+++ check-install.sh
@@ -7,6 +7,8 @@
 if [ "$1" = "mingw" ]; then
     SUFFIX=-mingw
     TRIPLE=x86_64-w64-mingw32
+    MESON_CPU=x86_64
+    MESON_CPU_FAMILY=x86_64
     CONFIG_FLAGS="--host=$TRIPLE"
     CMAKE_FLAGS="-DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=$TRIPLE-gcc -DCMAKE_CXX_COMPILER=$TRIPLE-g++"
     MINGW=1
@@ -49,11 +51,49 @@ ninja
 ninja install
 
 cd ..
+mkdir -p build-check-meson$SUFFIX
+cd build-check-meson$SUFFIX
+
+if [ -n "$MINGW" ]; then
+    cat <<EOF > cross.txt
+[binaries]
+c = '$TRIPLE-gcc'
+cpp = '$TRIPLE-g++'
+ar = '$TRIPLE-ar'
+strip = '$TRIPLE-strip'
+windres = '$TRIPLE-windres'
+
+[host_machine]
+system = 'windows'
+cpu_family = '$MESON_CPU_FAMILY'
+cpu = '$MESON_CPU'
+endian = 'little'
+EOF
+    MESON_FLAGS="--cross-file cross.txt"
+fi
+
+# Explicitly pass --libdir; on Linux, Meson defaults to installing into
+# <prefix>/lib/<arch> - see https://github.com/mesonbuild/meson/issues/5925.
+rm -rf meson-*
+meson setup .. \
+    --buildtype release \
+    --default-library both \
+    --prefix $(pwd)/../install-meson$SUFFIX \
+    --libdir $(pwd)/../install-meson$SUFFIX/lib \
+    $MESON_FLAGS
+
+ninja
+rm -rf ../install-meson$SUFFIX
+ninja install
+
+cd ..
 
 cd install-autotools$SUFFIX
 find . | sed 's/^..//' | sort | grep -v '\.la$' > ../listing-autotools.txt
 cd ../install-cmake$SUFFIX
 find . | sed 's/^..//' | sort | grep -v '^lib/cmake' > ../listing-cmake.txt
+cd ../install-meson$SUFFIX
+find . | sed 's/^..//' | sort > ../listing-meson.txt
 cd ..
 
 if [ "$(uname)" = "Darwin" ] && [ -z "$MINGW" ]; then
@@ -65,6 +105,7 @@ if [ "$(uname)" = "Darwin" ] && [ -z "$MINGW" ]; then
 fi
 
 diff -u listing-autotools.txt listing-cmake.txt
+diff -u listing-autotools.txt listing-meson.txt
 
 # Check the soname, or equivalent.
 if [ -n "$MINGW" ]; then
@@ -73,21 +114,28 @@ if [ -n "$MINGW" ]; then
     }
     SONAME_AUTOTOOLS=$(get_dllname install-autotools-mingw/lib/libfdk-aac.dll.a)
     SONAME_CMAKE=$(get_dllname install-cmake-mingw/lib/libfdk-aac.dll.a)
+    SONAME_MESON=$(get_dllname install-meson-mingw/lib/libfdk-aac.dll.a)
 elif [ "$(uname)" = "Darwin" ]; then
     get_soname() {
         basename $(otool -D $1 | tail -1)
     }
     SONAME_AUTOTOOLS=$(get_soname install-autotools/lib/libfdk-aac.dylib)
     SONAME_CMAKE=$(get_soname install-cmake/lib/libfdk-aac.dylib)
+    SONAME_MESON=$(get_soname install-meson/lib/libfdk-aac.dylib)
 else
     get_soname() {
         readelf -d $1 | grep SONAME | sed -e 's/.*soname: //' -e 's/^\[//' -e 's/\]$//'
     }
     SONAME_AUTOTOOLS=$(get_soname install-autotools/lib/libfdk-aac.so)
     SONAME_CMAKE=$(get_soname install-cmake/lib/libfdk-aac.so)
+    SONAME_MESON=$(get_soname install-meson/lib/libfdk-aac.so)
 fi
 
 if [ "$SONAME_AUTOTOOLS" != "$SONAME_CMAKE" ]; then
     echo Autotools SONAME $SONAME_AUTOTOOLS differs from CMake $SONAME_CMAKE
+    exit 1
+fi
+if [ "$SONAME_AUTOTOOLS" != "$SONAME_MESON" ]; then
+    echo Autotools SONAME $SONAME_AUTOTOOLS differs from Meson $SONAME_MESON
     exit 1
 fi
