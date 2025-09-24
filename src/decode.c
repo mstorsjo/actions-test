98df18194fb51eb0f246ec71482858723a518407
Ronald S. Bultje <rsbultje@gmail.com>
2025-09-24 16:14:01 -0400

    Fix GDF block num comparison to use the same unit on both sides.
    (Fixes GDF being parsed on 64x64 sequences.)

--- src/decode.c
+++ src/decode.c
@@ -1210,7 +1210,7 @@ static int decode_b(Dav1dTaskContext *const t, DB_ONLY(const int depth)
         const int gdf_bs = f->frame_hdr->frame_type == DAV1D_FRAME_TYPE_KEY ?
                            32 : imax(32, 16 << f->seq_hdr->sb128);
         if (!((t->bx | t->by) & (gdf_bs - 1)) &&
-            imax(f->cur.p.w, f->cur.p.h) > gdf_bs)
+            imax(f->cur.p.w, f->cur.p.h) > 4 * gdf_bs)
         {
             for (int y = 0; y < bh4; y += gdf_bs) {
                 for (int x = 0; x < bw4; x += gdf_bs) {
