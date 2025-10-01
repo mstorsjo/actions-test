a8fa13bb04d7f64967607d46f0a64b02e27ba294
Martin Storsjö <martin@martin.st>
2025-10-01 13:40:07 +0300

    Simplify the code further

--- exit1.c
+++ exit1.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
 #include <windows.h>
-
-int main(int argc, char * argv[])
-{
-  fprintf(stderr, "ExitThread\n"); fflush(stderr);
+int main(int argc, char *argv[]) {
   ExitThread(0);
-  fprintf(stderr, "not exited?\n"); fflush(stderr);
   return 1;
 }
