fd1fca552238bc59a9630b558e4a6eab5ced31e3
Ronald S. Bultje <rsbultje@gmail.com>
2025-10-17 08:54:25 -0400

    Fix amvd context.

--- src/decode.c
+++ src/decode.c
@@ -2165,7 +2165,9 @@ static int decode_b(Dav1dTaskContext *const t, DB_ONLY(const int depth)
             }
 
             if (f->seq_hdr->adaptive_mvd && b->inter_mode == NEWMV) {
-                const int ctx = nx[0]->amvd[xoff[0]] + nx[1]->amvd[xoff[1]];
+                const int ctx =
+                    (nx[0]->amvd[xoff[0]] && nx[0]->ref[0][xoff[0]] == b->ref[0]) +
+                    (nx[1]->amvd[xoff[1]] && nx[1]->ref[0][xoff[1]] == b->ref[0]);
                 amvd = dav1d_msac_decode_bool_adapt(&ts->msac,
                                                     ts->cdf.m.amvd[4][ctx]);
                 mvprec_def = !amvd || f->frame_hdr->mv_precision < 3;
