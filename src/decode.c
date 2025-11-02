47008866c9d134131f20776633ef8576a667faaf
Ronald S. Bultje <rsbultje@gmail.com>
2025-11-02 15:53:22 +0000

    Fix amvd context for compound prediction.
    Like fd1fca552238bc59a9630b558e4a6eab5ced31e3 but for comp-pred.

--- src/decode.c
+++ src/decode.c
@@ -2058,7 +2058,9 @@ static int decode_b(Dav1dTaskContext *const t, DB_ONLY(const int depth)
                     [OPFL_NEWMV_NEWMV  - NEARMV_NEWMV] = 8,
                 };
                 const int mode_ctx = amvd_mode_context[b->inter_mode - NEARMV_NEWMV];
-                const int ctx = nx[0]->amvd[xoff[0]] + nx[1]->amvd[xoff[1]];
+                const int ctx =
+                    (nx[0]->amvd[xoff[0]] && nx[0]->ref[0][xoff[0]] == b->ref[0]) +
+                    (nx[1]->amvd[xoff[1]] && nx[1]->ref[0][xoff[1]] == b->ref[0]);
                 amvd = dav1d_msac_decode_bool_adapt(&ts->msac,
                                                     ts->cdf.m.amvd[mode_ctx][ctx]);
                 mvprec_def = 2 - (!amvd || f->frame_hdr->mv_precision < 3);
