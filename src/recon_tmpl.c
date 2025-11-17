450a367cbc5c2a933473ff2b4b7508137568bdc7
Luc Trudeau <ltrudeau@twoorioles.com>
2025-11-17 08:59:05 -0500

    Remove intra mode check

--- src/recon_tmpl.c
+++ src/recon_tmpl.c
@@ -1405,12 +1405,7 @@ static void recon_b_luma_tx(Dav1dTaskContext *const t, DB_ONLY(const int depth)
                              imin(t_dim->h, f->bh - t->by));
     t->scratch.txtp_map[(t->by & 15) * 16 + (t->bx & 15)] = txtp & 0xf;
 
-    if (b->y_mode == DC_PRED || b->y_mode == VERT_PRED || b->y_mode == HOR_PRED ||
-        b->y_mode == HOR_UP_PRED ||
-        b->y_mode == DIAG_DOWN_RIGHT_PRED || b->y_mode == PAETH_PRED ||
-        b->y_mode == VERT_LEFT_PRED ||
-        (b->y_mode >= SMOOTH_PRED && b->y_mode <= SMOOTH_H_PRED))
-    {
+    if (b->intra && !b->intrabc && !b->pal_sz) {
         const int mrl_idx = b->mrl_index;
         const int mrl_mul = b->multi_mrl;
         pixel *const edge = bitfn(t->scratch.edge) + (mrl_idx ? 384 : 128);
