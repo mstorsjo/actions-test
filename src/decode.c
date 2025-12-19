59fae48922539be76feaeee803554263fc1b6a33
Victorien Le Couviour--Tuffet <victorien@videolan.org>
2025-12-19 13:51:33 +0100

    msan: Adapt checked_decode_b

--- src/decode.c
+++ src/decode.c
@@ -3201,42 +3201,42 @@ static int decode_b(Dav1dTaskContext *const t, DB_ONLY(const int depth)
 
 #include <sanitizer/msan_interface.h>
 
-static int checked_decode_b(Dav1dTaskContext *const t, const enum BlockSize bs) {
+static int checked_decode_b(Dav1dTaskContext *const t, DB_ONLY(const int depth)
+                            const enum BlockSize lbs, const enum BlockSize cbs)
+{
     const Dav1dFrameContext *const f = t->f;
-    const int err = decode_b(t, bs);
+    const int err = decode_b(t, DB_ONLY(depth) lbs, cbs);
+    enum BlockSize bs[2] = { lbs, cbs };
 
-    if (err == 0 && !(t->frame_thread.pass & 1)) {
-        const int ss_ver = f->cur.p.layout == DAV1D_PIXEL_LAYOUT_I420;
-        const int ss_hor = f->cur.p.layout != DAV1D_PIXEL_LAYOUT_I444;
-        const uint8_t *const b_dim = dav1d_block_dimensions[bs];
-        const int bw4 = b_dim[0], bh4 = b_dim[1];
-        const int w4 = imin(bw4, f->bw - t->bx), h4 = imin(bh4, f->bh - t->by);
-        const int has_chroma = f->seq_hdr->layout != DAV1D_PIXEL_LAYOUT_I400 &&
-                               (bw4 > ss_hor || t->bx & 1) &&
-                               (bh4 > ss_ver || t->by & 1);
+    if (err == 0 && !(t->frame_thread.pass & 1))
+        for (int i = 0; i < 2; i++) {
+            if (bs[i] == BS_INVALID) continue;
+            const uint8_t *const b_dim = dav1d_block_dimensions[bs[i]];
+            const int bw4 = b_dim[0], bh4 = b_dim[1];
+            const int w4 = imin(bw4, f->bw - t->bx), h4 = imin(bh4, f->bh - t->by);
+            const ptrdiff_t stride = f->cur.stride[i];
+            int bx = i ? t->cbx : t->bx, by = i ? t->cby : t->by;
 
-        for (int p = 0; p < 1 + 2 * has_chroma; p++) {
-            const int ss_ver = p && f->cur.p.layout == DAV1D_PIXEL_LAYOUT_I420;
-            const int ss_hor = p && f->cur.p.layout != DAV1D_PIXEL_LAYOUT_I444;
-            const ptrdiff_t stride = f->cur.stride[!!p];
-            const int bx = t->bx & ~ss_hor;
-            const int by = t->by & ~ss_ver;
-            const int width  = w4 << (2 - ss_hor + (bw4 == ss_hor));
-            const int height = h4 << (2 - ss_ver + (bh4 == ss_ver));
+            for (int p = i; p < 1 + i * 2; p++) {
+                const int ss_ver = p && f->cur.p.layout == DAV1D_PIXEL_LAYOUT_I420;
+                const int ss_hor = p && f->cur.p.layout != DAV1D_PIXEL_LAYOUT_I444;
+                const int width  = w4 << (2 - ss_hor + (bw4 == ss_hor));
+                const int height = h4 << (2 - ss_ver + (bh4 == ss_ver));
+                bx &= ~ss_hor; by &= ~ss_ver;
 
-            const uint8_t *data = f->cur.data[p] + (by << (2 - ss_ver)) * stride +
-                                  (bx << (2 - ss_hor + !!f->seq_hdr->hbd));
+                const uint8_t *data = f->cur.data[p] + (by << (2 - ss_ver)) * stride +
+                                      (bx << (2 - ss_hor + !!f->seq_hdr->hbd));
 
-            for (int y = 0; y < height; data += stride, y++) {
-                const size_t line_sz = width << !!f->seq_hdr->hbd;
-                if (__msan_test_shadow(data, line_sz) != -1) {
-                    fprintf(stderr, "B[%d](%d, %d) w4:%d, h4:%d, row:%d\n",
-                            p, bx, by, w4, h4, y);
-                    __msan_check_mem_is_initialized(data, line_sz);
+                for (int y = 0; y < height; data += stride, y++) {
+                    const size_t line_sz = width << !!f->seq_hdr->hbd;
+                    if (__msan_test_shadow(data, line_sz) != -1) {
+                        fprintf(stderr, "B[%d](%d, %d) w4:%d, h4:%d, row:%d\n",
+                                p, bx, by, w4, h4, y);
+                        __msan_check_mem_is_initialized(data, line_sz);
+                    }
                 }
             }
         }
-    }
 
     return err;
 }
