mstorsjo/actions-test

commit bc1b93fc5625b5f72c00412191ab5e5b752d15e7

Add basic intra chroma recon
Does intra prediction (av1 predictors minus cfl) and applies inverse transform.

src/ipred_tmpl.c
@@ -573,6 +573,7 @@ static void ipred_z1_c(pixel *dst, const ptrdiff_t stride,
                        HIGHBD_DECL_SUFFIX)
 {
     const int angle_flags = angle & ~(511 | ANGLE_IBP_FLAG);
+    const int is_luma = angle & ANGLE_IS_LUMA;
     const int is_sm_t = !!(angle & ANGLE_SMOOTH_TOP_EDGE_FLAG);
     const int enable_intra_edge_filter = !!(angle & ANGLE_USE_EDGE_FILTER_FLAG);
     const int enable_ibp = !!(angle & ANGLE_IBP_FLAG);
@@ -586,11 +587,12 @@ static void ipred_z1_c(pixel *dst, const ptrdiff_t stride,
         const int e_stride = (width + height + (mrl_idx << 1) + 3) * 2;
         const pixel *tl2 = &topleft_in[-e_stride];
         pixel tmp[64 * 64];
+        assert(is_luma);
         ipred_z1_c(tmp, 64 * sizeof(pixel), topleft_in, width, height,
-                   angle | (mrl_idx << ANGLE_MRL_IDX_SHIFT),
+                   angle | (mrl_idx << ANGLE_MRL_IDX_SHIFT) | ANGLE_IS_LUMA,
                    max_width, max_height HIGHBD_TAIL_SUFFIX);
-        ipred_z1_c(dst, stride, tl2, width, height,
-                   angle, max_width, max_height HIGHBD_TAIL_SUFFIX);
+        ipred_z1_c(dst, stride, tl2, width, height, angle | ANGLE_IS_LUMA,
+                   max_width, max_height HIGHBD_TAIL_SUFFIX);
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++)
                 dst[x] = (tmp[y * 64 + x] + dst[x]) >> 1;
@@ -631,16 +633,22 @@ static void ipred_z1_c(pixel *dst, const ptrdiff_t stride,
             }
             break;
         }
-        const DRFilter4Tap f = av1_dr_interp_filter[(xpos & 0x3F) >> 1];
+        const int shift = (xpos & 0x3F) >> 1;
+        const DRFilter4Tap f = av1_dr_interp_filter[shift];
         for (int x = 0; x < width; x++, base++) {
-            if (base > max_base_x) {
+            if (base >= max_base_x) {
                 pixel_set(&dst[y * PXSTRIDE(stride) + x], top[max_base_x],
                           width - x);
                 break;
             }
-            const int v = f.a * top[base - 1] + f.b * top[base] +
-                          f.c * top[base + 1] + f.d * top[base + 2];
-            dst[y * PXSTRIDE(stride) + x] = iclip_pixel((v + 64) >> 7);
+            if (is_luma) {
+                const int v = f.a * top[base - 1] + f.b * top[base] +
+                              f.c * top[base + 1] + f.d * top[base + 2];
+                dst[y * PXSTRIDE(stride) + x] = iclip_pixel((v + 64) >> 7);
+            } else {
+                const int v = (32 - shift) * top[base] + shift * top[base + 1];
+                dst[y * PXSTRIDE(stride) + x] = iclip_pixel((v + 16) >> 5);
+            }
         }
     }
 
@@ -673,6 +681,7 @@ static void ipred_z2_c(pixel *dst, const ptrdiff_t stride,
                        HIGHBD_DECL_SUFFIX)
 {
     const int mrl_mul = !!(angle & ANGLE_MULTI_MRL_FLAG);
+    const int is_luma = angle & ANGLE_IS_LUMA;
     const int is_sm_l = !!(angle & ANGLE_SMOOTH_LEFT_EDGE_FLAG);
     const int is_sm_t = !!(angle & ANGLE_SMOOTH_TOP_EDGE_FLAG);
     const int enable_intra_edge_filter = !!(angle & ANGLE_USE_EDGE_FILTER_FLAG);
@@ -686,11 +695,12 @@ static void ipred_z2_c(pixel *dst, const ptrdiff_t stride,
         const int e_stride = (width + height + (mrl_idx << 1) + 3) * 2;
         const pixel *tl2 = &topleft_in[-e_stride];
         pixel tmp[64 * 64];
+        assert(is_luma);
         ipred_z2_c(tmp, 64 * sizeof(pixel), topleft_in, width, height,
-                   angle | (mrl_idx << ANGLE_MRL_IDX_SHIFT),
+                   angle | (mrl_idx << ANGLE_MRL_IDX_SHIFT) | ANGLE_IS_LUMA,
                    max_width, max_height HIGHBD_TAIL_SUFFIX);
-        ipred_z2_c(dst, stride, tl2, width, height,
-                   angle, max_width, max_height HIGHBD_TAIL_SUFFIX);
+        ipred_z2_c(dst, stride, tl2, width, height, angle | ANGLE_IS_LUMA,
+                   max_width, max_height HIGHBD_TAIL_SUFFIX);
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++)
                 dst[x] = (tmp[y * 64 + x] + dst[x]) >> 1;
@@ -744,23 +754,35 @@ static void ipred_z2_c(pixel *dst, const ptrdiff_t stride,
             const int base_y = ypos_l >> 6;
             assert(base_y >= -(1 + mrl_idx));
             const int shift = (ypos_l & 0x3F) >> 1;
-            const int v =
-                av1_dr_interp_filter[shift].a * left[-(base_y + 1)] +
-                av1_dr_interp_filter[shift].b * left[-(base_y + 2)] +
-                av1_dr_interp_filter[shift].c * left[-(base_y + 3)] +
-                av1_dr_interp_filter[shift].d * left[-(base_y + 4)];
-            dst[x] = iclip_pixel((v + 64) >> 7);
+            if (is_luma) {
+                const int v =
+                    av1_dr_interp_filter[shift].a * left[-(base_y + 1)] +
+                    av1_dr_interp_filter[shift].b * left[-(base_y + 2)] +
+                    av1_dr_interp_filter[shift].c * left[-(base_y + 3)] +
+                    av1_dr_interp_filter[shift].d * left[-(base_y + 4)];
+                dst[x] = iclip_pixel((v + 64) >> 7);
+            } else {
+                const int v = (32 - shift) * left[-(base_y + 2)] +
+                              shift * left[-(base_y + 3)];
+                dst[x] = iclip_pixel((v + 16) >> 5);
+            }
         }
 
         for (; x < width; x++, xpos += 64) {
             const int base_x = xpos >> 6;
             const int shift = (xpos & 0x3F) >> 1;
-            const int v =
-                av1_dr_interp_filter[shift].a * top[base_x + 1] +
-                av1_dr_interp_filter[shift].b * top[base_x + 2] +
-                av1_dr_interp_filter[shift].c * top[base_x + 3] +
-                av1_dr_interp_filter[shift].d * top[base_x + 4];
-            dst[x] = iclip_pixel((v + 64) >> 7);
+            if (is_luma) {
+                const int v =
+                    av1_dr_interp_filter[shift].a * top[base_x + 1] +
+                    av1_dr_interp_filter[shift].b * top[base_x + 2] +
+                    av1_dr_interp_filter[shift].c * top[base_x + 3] +
+                    av1_dr_interp_filter[shift].d * top[base_x + 4];
+                dst[x] = iclip_pixel((v + 64) >> 7);
+            } else {
+                const int v = (32 - shift) * top[base_x + 2] +
+                              shift * top[base_x + 3];
+                dst[x] = iclip_pixel((v + 16) >> 5);
+            }
         }
         dst += PXSTRIDE(stride);
     }
@@ -773,6 +795,7 @@ static void ipred_z3_c(pixel *dst, const ptrdiff_t stride,
                        HIGHBD_DECL_SUFFIX)
 {
     const int angle_flags = angle & ~(511 | ANGLE_IBP_FLAG);
+    const int is_luma = angle & ANGLE_IS_LUMA;
     const int is_sm_l = !!(angle & ANGLE_SMOOTH_LEFT_EDGE_FLAG);
     const int enable_intra_edge_filter = !!(angle & ANGLE_USE_EDGE_FILTER_FLAG);
     const int have_left = !!(angle & ANGLE_HAS_LEFT_FLAG);
@@ -787,11 +810,12 @@ static void ipred_z3_c(pixel *dst, const ptrdiff_t stride,
         const int e_stride = (width + height + (mrl_idx << 1) + 3) * 2;
         const pixel *tl2 = &topleft_in[-e_stride];
         pixel tmp[64 * 64];
+        assert(is_luma);
         ipred_z3_c(tmp, 64 * sizeof(pixel), topleft_in, width, height,
-                   angle | (mrl_idx << ANGLE_MRL_IDX_SHIFT),
+                   angle | (mrl_idx << ANGLE_MRL_IDX_SHIFT) | ANGLE_IS_LUMA,
                    max_width, max_height HIGHBD_TAIL_SUFFIX);
-        ipred_z3_c(dst, stride, tl2, width, height,
-                   angle, max_width, max_height HIGHBD_TAIL_SUFFIX);
+        ipred_z3_c(dst, stride, tl2, width, height, angle | ANGLE_IS_LUMA,
+                   max_width, max_height HIGHBD_TAIL_SUFFIX);
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++)
                 dst[x] = (tmp[y * 64 + x] + dst[x]) >> 1;
@@ -828,12 +852,19 @@ static void ipred_z3_c(pixel *dst, const ptrdiff_t stride,
 
     int ypos = dy * (1 + mrl_idx);
     for (int x = 0; x < width; x++, ypos += dy) {
-        const DRFilter4Tap f = av1_dr_interp_filter[(ypos & 0x3F) >> 1];
+        const int shift = (ypos & 0x3F) >> 1;
+        const DRFilter4Tap f = av1_dr_interp_filter[shift];
         for (int y = 0, base = ypos >> 6; y < height; y++, base++) {
             if (base <= max_base_y) {
-                const int v = f.a * left[-(base - 1)] + f.b * left[-base] +
-                              f.c * left[-(base + 1)] + f.d * left[-(base + 2)];
-                dst[y * PXSTRIDE(stride) + x] = iclip_pixel((v + 64) >> 7);
+                if (is_luma) {
+                    const int v = f.a * left[-(base - 1)] + f.b * left[-base] +
+                                  f.c * left[-(base + 1)] + f.d * left[-(base + 2)];
+                    dst[y * PXSTRIDE(stride) + x] = iclip_pixel((v + 64) >> 7);
+                } else {
+                    const int v = (32 - shift) * left[-base] +
+                                  shift * left[-(base + 1)];
+                    dst[y * PXSTRIDE(stride) + x] = iclip_pixel((v + 16) >> 5);
+                }
             } else {
                 do {
                     dst[y * PXSTRIDE(stride) + x] = left[-max_base_y];

patches/0004-Print-Intra-Prediction-DQ-coeffs-and-inverse-transfo.patch
@@ -1,21 +1,21 @@
-From 0f9e594884aab69236299788d001c643734f855d Mon Sep 17 00:00:00 2001
+From 30066938be7b846b97d31421162c1f7833534702 Mon Sep 17 00:00:00 2001
 From: Luc Trudeau <ltrudeau@twoorioles.com>
 Date: Wed, 17 Sep 2025 16:53:29 -0400
-Subject: [PATCH 4/5] Print Intra Prediction, DQ coeffs and inverse transform
+Subject: [PATCH 1/2] Print Intra Prediction, DQ coeffs and inverse transform
 
 ---
  aom/debug.h               |  49 ++++++++++++
  av1/common/idct.c         |  20 ++++-
  av1/common/idct.h         |   7 +-
- av1/common/reconintra.c   | 156 ++++++++++++++++++++++++++++++++++++--
- av1/decoder/decodeframe.c |  27 ++++++-
+ av1/common/reconintra.c   | 152 ++++++++++++++++++++++++++++++++++++--
+ av1/decoder/decodeframe.c |  36 ++++++++-
  av1/encoder/encodemb.c    |  10 +--
  av1/encoder/tpl_model.c   |   2 +-
  av1/encoder/tx_search.c   |   8 +-
- 8 files changed, 257 insertions(+), 22 deletions(-)
+ 8 files changed, 262 insertions(+), 22 deletions(-)
 
 diff --git a/aom/debug.h b/aom/debug.h
-index b203c56cf..d13c4174a 100644
+index b203c56..d13c417 100644
 --- a/aom/debug.h
 +++ b/aom/debug.h
 @@ -28,10 +28,15 @@
@@ -86,7 +86,7 @@ index b203c56cf..d13c4174a 100644
  #define DB_ONLY(x...)
  #define BLOCK_TO_DEBUG 0
 diff --git a/av1/common/idct.c b/av1/common/idct.c
-index 958b80d9b..f970e6370 100644
+index 958b80d..f970e63 100644
 --- a/av1/common/idct.c
 +++ b/av1/common/idct.c
 @@ -995,7 +995,8 @@ static void av1_highbd_inv_txfm_add_master(const tran_low_t *input,
@@ -151,7 +151,7 @@ index 958b80d9b..f970e6370 100644
    }
  }
 diff --git a/av1/common/idct.h b/av1/common/idct.h
-index 419319c47..d97b0a9cd 100644
+index 419319c..d97b0a9 100644
 --- a/av1/common/idct.h
 +++ b/av1/common/idct.h
 @@ -21,6 +21,8 @@
@@ -183,7 +183,7 @@ index 419319c47..d97b0a9cd 100644
  #ifdef __cplusplus
  }  // extern "C"
 diff --git a/av1/common/reconintra.c b/av1/common/reconintra.c
-index 68d4b6f03..ae16680d3 100644
+index 68d4b6f..d735196 100644
 --- a/av1/common/reconintra.c
 +++ b/av1/common/reconintra.c
 @@ -27,6 +27,8 @@
@@ -204,7 +204,7 @@ index 68d4b6f03..ae16680d3 100644
      const MACROBLOCKD *xd, const uint16_t *ref, int ref_stride, uint16_t *dst,
      int dst_stride, PREDICTION_MODE mode, int angle_delta, TX_SIZE tx_size,
      int disable_edge_filter, int n_top_px, int n_topright_px, int n_left_px,
-@@ -1135,6 +1137,12 @@ static void build_intra_predictors_high(
+@@ -1135,6 +1137,15 @@ static void build_intra_predictors_high(
    uint16_t *const above_row_2nd = above_data_2nd + 32;
    uint16_t *const left_col_2nd = left_data_2nd + 32;
  
@@ -213,22 +213,24 @@ index 68d4b6f03..ae16680d3 100644
 +  const int mi_col = xd->mi_col;
 +  const int hbd = !!(xd->bd - 8);
 +#endif
++#if DEBUG_B_PIXELS
++  const char *pred_names[] = { "y-intra-pred", "u-intra-pred", "v-intra-pred" };
++#endif
 +
    uint16_t *const second_pred = second_pred_data + 16;
    uint16_t *dst_mrl_line_0 = mrl_line_0_data + 16;
    const int txwpx = tx_size_wide[tx_size];
-@@ -1224,20 +1232,41 @@ static void build_intra_predictors_high(
+@@ -1224,20 +1235,40 @@ static void build_intra_predictors_high(
        val = (n_left_px > 0) ? left_ref_1st[0] : base - 1;
      }
      for (i = 0; i < txhpx; ++i) {
 -      aom_memset16(dst, val, txwpx);
 -      dst += dst_stride;
 +      aom_memset16(&dst[i * dst_stride], val, txwpx);
-     }
++    }
 +#if DEBUG_B_PIXELS
 +    if (BLOCK_TO_DEBUG) {
-+      // FIXME only print luma for now
-+      if (plane == AOM_PLANE_Y) {
++      if (0 || plane == AOM_PLANE_Y) {
 +        if (need_left) {
 +          int num_left_pixels_needed = txhpx + 3 + (mrl_index << 1);
 +          printf("l\n");
@@ -242,9 +244,9 @@ index 68d4b6f03..ae16680d3 100644
 +            printf(" %02x", val);
 +          printf("\n");
 +        }
-+        hex_dump(dst, dst_stride, txwpx, txhpx, "y-intra-pred", hbd);
++        hex_dump(dst, dst_stride, txwpx, txhpx, pred_names[plane], hbd);
 +      }
-+    }
+     }
 +#endif
      return;
    }
@@ -262,7 +264,7 @@ index 68d4b6f03..ae16680d3 100644
          txhpx + (need_bottom ? txwpx : 3) + (mrl_index << 1);
      if (use_intra_dip) {
        // DIP mode requires left edge + 1/4 tx height for overhang feature.
-@@ -1269,12 +1298,13 @@ static void build_intra_predictors_high(
+@@ -1269,12 +1300,13 @@ static void build_intra_predictors_high(
    }
  
    // NEED_ABOVE
@@ -277,7 +279,7 @@ index 68d4b6f03..ae16680d3 100644
          txwpx + (need_right ? txhpx : 0) + (mrl_index << 1);
      if (use_intra_dip) {
        // DIP mode requires above line + 1/4 tx width for overhang feature.
-@@ -1329,6 +1359,73 @@ static void build_intra_predictors_high(
+@@ -1329,6 +1361,72 @@ static void build_intra_predictors_high(
      }
    }
  
@@ -306,8 +308,7 @@ index 68d4b6f03..ae16680d3 100644
 +              print_t = 0;
 +      }
 +    const char *pix_fmt = hbd ? " %03x" : " %02x";
-+  // FIXME only print luma for now
-+    if (plane == AOM_PLANE_Y) {
++    if (0 || plane == AOM_PLANE_Y) {
 +      if (print_l) {
 +        printf("l\n");
 +        for (int x = num_left_pixels_needed - 1; x >= 0; x--)
@@ -351,30 +352,28 @@ index 68d4b6f03..ae16680d3 100644
    if (use_intra_dip) {
      av1_highbd_intra_dip_predictor(mbmi->intra_dip_mode, dst, dst_stride,
                                     above_row_1st, left_col_1st, tx_size, xd->bd
-@@ -1337,6 +1434,14 @@ static void build_intra_predictors_high(
+@@ -1337,6 +1435,13 @@ static void build_intra_predictors_high(
                                     mbmi->intra_dip_features
  #endif  // CONFIG_DIP_EXT_PRUNING
      );
 +#if DEBUG_B_PIXELS
 +    if (BLOCK_TO_DEBUG) {
-+      // FIXME only print luma for now
-+      if (plane == AOM_PLANE_Y) {
-+        hex_dump(dst, dst_stride, txwpx, txhpx, "y-intra-pred", hbd);
++      if (0 || plane == AOM_PLANE_Y) {
++        hex_dump(dst, dst_stride, txwpx, txhpx, pred_names[plane], hbd);
 +      }
 +    }
 +#endif
      return;
    }
  
-@@ -1458,11 +1563,28 @@ static void build_intra_predictors_high(
+@@ -1458,11 +1563,26 @@ static void build_intra_predictors_high(
        }
      }
  
 +#if DEBUG_B_PIXELS
 +    if (BLOCK_TO_DEBUG) {
-+      // FIXME only print luma for now
-+      if (plane == AOM_PLANE_Y) {
-+        hex_dump(dst, dst_stride, txwpx, txhpx, "y-intra-pred", hbd);
++      if (0 || plane == AOM_PLANE_Y) {
++        hex_dump(dst, dst_stride, txwpx, txhpx, pred_names[plane], hbd);
 +      }
 +    }
 +#endif
@@ -386,8 +385,7 @@ index 68d4b6f03..ae16680d3 100644
                                       left_col_1st, mode, xd->bd);
 +#if DEBUG_B_PIXELS
 +      if (BLOCK_TO_DEBUG) {
-+        // FIXME only print luma for now
-+        if (plane == AOM_PLANE_Y) {
++        if (0 || plane == AOM_PLANE_Y) {
 +          hex_dump(dst, dst_stride, txwpx, txhpx, "orip", hbd);
 +        }
 +      }
@@ -395,29 +393,27 @@ index 68d4b6f03..ae16680d3 100644
      }
      return;
    }
-@@ -1485,6 +1607,14 @@ static void build_intra_predictors_high(
+@@ -1485,6 +1605,13 @@ static void build_intra_predictors_high(
      pred_high[mode][tx_size](dst, dst_stride, above_row_1st, left_col_1st,
                               xd->bd);
    }
 +#if DEBUG_B_PIXELS
 +  if (BLOCK_TO_DEBUG) {
-+    // FIXME only print luma for now
-+    if (plane == AOM_PLANE_Y) {
-+      hex_dump(dst, dst_stride, txwpx, txhpx, "y-intra-pred", hbd);
++    if (0 || plane == AOM_PLANE_Y) {
++      hex_dump(dst, dst_stride, txwpx, txhpx, pred_names[plane], hbd);
 +    }
 +  }
 +#endif
  
    // Apply sub-block based filter for DC/smooth intra mode
    apply_sub_block_based_refinement_filter &=
-@@ -1492,6 +1622,14 @@ static void build_intra_predictors_high(
+@@ -1492,6 +1619,13 @@ static void build_intra_predictors_high(
    if (apply_sub_block_based_refinement_filter) {
      av1_apply_orip_4x4subblock_hbd(dst, dst_stride, tx_size, above_row_1st,
                                     left_col_1st, mode, xd->bd);
 +#if DEBUG_B_PIXELS
 +    if (BLOCK_TO_DEBUG) {
-+      // FIXME only print luma for now
-+      if (plane == AOM_PLANE_Y) {
++      if (0 || plane == AOM_PLANE_Y) {
 +        hex_dump(dst, dst_stride, txwpx, txhpx, "orip", hbd);
 +      }
 +    }
@@ -425,7 +421,7 @@ index 68d4b6f03..ae16680d3 100644
    }
  }
  
-@@ -1521,6 +1659,12 @@ void av1_predict_intra_block(const AV1_COMMON *cm, const MACROBLOCKD *xd,
+@@ -1521,6 +1655,12 @@ void av1_predict_intra_block(const AV1_COMMON *cm, const MACROBLOCKD *xd,
          dst[r * dst_stride + c] = palette[map[(r + y) * wpx + c + x]];
        }
      }
@@ -438,7 +434,7 @@ index 68d4b6f03..ae16680d3 100644
      return;
    }
  
-@@ -1575,7 +1719,7 @@ void av1_predict_intra_block(const AV1_COMMON *cm, const MACROBLOCKD *xd,
+@@ -1575,7 +1715,7 @@ void av1_predict_intra_block(const AV1_COMMON *cm, const MACROBLOCKD *xd,
    const int is_sb_boundary =
        (mi_row % cm->mib_size == 0 && row_off == 0) ? 1 : 0;
  
@@ -448,7 +444,7 @@ index 68d4b6f03..ae16680d3 100644
        disable_edge_filter, have_top ? AOMMIN(txwpx, xr + txwpx) : 0,
        have_top_right ? px_top_right : 0,
 diff --git a/av1/decoder/decodeframe.c b/av1/decoder/decodeframe.c
-index a9dff1b78..8f394dd1a 100644
+index a9dff1b..e6f4c89 100644
 --- a/av1/decoder/decodeframe.c
 +++ b/av1/decoder/decodeframe.c
 @@ -212,11 +212,25 @@ static AOM_INLINE void inverse_transform_block(DecoderCodingBlock *dcb,
@@ -459,26 +455,42 @@ index a9dff1b78..8f394dd1a 100644
 +#if DEBUG_BLOCK_INFO
 +  const int mi_row = dcb->xd.mi_row;
 +  const int mi_col = dcb->xd.mi_col;
-+  const int print = BLOCK_TO_DEBUG && DEBUG_B_PIXELS && plane == AOM_PLANE_Y;
-+  if (print && !(tx_type >> 4)) {
++  const int print = BLOCK_TO_DEBUG && DEBUG_B_PIXELS;
++  if (print && (0 || plane == AOM_PLANE_Y) && !(tx_type >> 4)) {
 +    coef_dump_transpose(dqcoeff, AOMMIN(tx_size_wide[tx_size], 32),
 +                        AOMMIN(tx_size_high[tx_size], 32), 3, "dq");
 +  }
 +#endif
-+  av1_inverse_transform_block(DB_ONLY(print)
++  av1_inverse_transform_block(DB_ONLY(print && (0 || plane == AOM_PLANE_Y))
        &dcb->xd, dqcoeff, plane, tx_type, tx_size, dst, stride, eob,
        replace_adst_by_ddt(cm->seq_params.enable_inter_ddt,
                            cm->features.allow_screen_content_tools, &dcb->xd),
        reduced_tx_set);
 +#if DEBUG_BLOCK_INFO
-+  if (print) {
++  if (print && plane == AOM_PLANE_Y) {
 +    hex_dump(dst, stride, tx_size_wide[tx_size], tx_size_high[tx_size], "recon", !!(dcb->xd.bd - 8));
 +  }
 +#endif
    const int width = tx_size_wide[tx_size] <= 32 ? tx_size_wide[tx_size] : 32;
    const int height = tx_size_high[tx_size] <= 32 ? tx_size_high[tx_size] : 32;
    const int sbSize = (width >= 8 && height >= 8) ? 8 : 4;
-@@ -952,6 +966,17 @@ static AOM_INLINE void predict_inter_block(AV1_COMMON *const cm,
+@@ -381,6 +395,15 @@ static AOM_INLINE void predict_and_reconstruct_intra_block(DB_ONLY(const int dep
+                               pd->dst.stride, reduced_tx_set_used);
+     }
+   }
++#if DEBUG_BLOCK_INFO && DEBUG_B_PIXELS
++  const int mi_row = dcb->xd.mi_row;
++  const int mi_col = dcb->xd.mi_col;
++  if (0 && BLOCK_TO_DEBUG && plane != AOM_PLANE_Y) {
++    struct macroblockd_plane *const pd = &xd->plane[plane];
++    uint16_t *dst = &pd->dst.buf[(row * pd->dst.stride + col) << MI_SIZE_LOG2];
++    hex_dump(dst, pd->dst.stride, tx_size_wide[tx_size], tx_size_high[tx_size], "recon", !!(dcb->xd.bd - 8));
++  }
++#endif
+ 
+ #if CONFIG_MISMATCH_DEBUG
+   {
+@@ -952,6 +975,17 @@ static AOM_INLINE void predict_inter_block(AV1_COMMON *const cm,
      }
    }
  #endif  // CONFIG_INSPECTION
@@ -497,7 +509,7 @@ index a9dff1b78..8f394dd1a 100644
  
  static AOM_INLINE void copy_frame_mvs_inter_block(AV1_COMMON *const cm,
 diff --git a/av1/encoder/encodemb.c b/av1/encoder/encodemb.c
-index 9877e2513..6b509f24e 100644
+index 9877e25..6b509f2 100644
 --- a/av1/encoder/encodemb.c
 +++ b/av1/encoder/encodemb.c
 @@ -1044,7 +1044,7 @@ static void encode_block(int plane, int block, int blk_row, int blk_col,
@@ -544,7 +556,7 @@ index 9877e2513..6b509f24e 100644
          AOMMAX(*eob_c1, *eob_c2),
          replace_adst_by_ddt(cm->seq_params.enable_inter_ddt,
 diff --git a/av1/encoder/tpl_model.c b/av1/encoder/tpl_model.c
-index 10de3e6ad..98ecd5edc 100644
+index 10de3e6..98ecd5e 100644
 --- a/av1/encoder/tpl_model.c
 +++ b/av1/encoder/tpl_model.c
 @@ -187,7 +187,7 @@ static AOM_INLINE void txfm_quant_rdcost(
@@ -557,7 +569,7 @@ index 10de3e6ad..98ecd5edc 100644
  }
  
 diff --git a/av1/encoder/tx_search.c b/av1/encoder/tx_search.c
-index 2c2ca1210..12b799051 100644
+index 2c2ca12..12b7990 100644
 --- a/av1/encoder/tx_search.c
 +++ b/av1/encoder/tx_search.c
 @@ -922,7 +922,7 @@ static AOM_INLINE void inverse_transform_block_facade(
@@ -595,5 +607,5 @@ index 2c2ca1210..12b799051 100644
        max_chroma_eob,
        replace_adst_by_ddt(cpi->common.seq_params.enable_inter_ddt,
 -- 
-2.39.5 (Apple Git-154)
+2.52.0
 

patches/0005-Add-loopfilters-to-enable-disable-loopfilters.patch
@@ -1,7 +1,7 @@
-From d4edd15147e81e1151f50b32611f9f665db8c2e7 Mon Sep 17 00:00:00 2001
+From dc28769152bee31a354411d913a756bbe92d93e5 Mon Sep 17 00:00:00 2001
 From: Luc Trudeau <ltrudeau@twoorioles.com>
 Date: Fri, 28 Nov 2025 09:52:45 -0500
-Subject: [PATCH 5/5] Add --loopfilters to enable/disable loopfilters
+Subject: [PATCH 2/2] Add --loopfilters to enable/disable loopfilters
 
 ---
  aom/aom_codec.h           | 10 ++++++++++
@@ -13,7 +13,7 @@ Subject: [PATCH 5/5] Add --loopfilters to enable/disable loopfilters
  6 files changed, 88 insertions(+), 7 deletions(-)
 
 diff --git a/aom/aom_codec.h b/aom/aom_codec.h
-index a338ed5614..5cda54d2d6 100644
+index a338ed5..5cda54d 100644
 --- a/aom/aom_codec.h
 +++ b/aom/aom_codec.h
 @@ -635,6 +635,16 @@ typedef enum {
@@ -34,7 +34,7 @@ index a338ed5614..5cda54d2d6 100644
   *
   * \param[in]     type            The OBU_TYPE to convert to string.
 diff --git a/aom/aomdx.h b/aom/aomdx.h
-index b5d454c67d..8adc78cbbf 100644
+index b5d454c..8adc78c 100644
 --- a/aom/aomdx.h
 +++ b/aom/aomdx.h
 @@ -342,6 +342,14 @@ enum aom_dec_control_id {
@@ -63,7 +63,7 @@ index b5d454c67d..8adc78cbbf 100644
  #define AOM_CTRL_AV1D_SET_BRU_OPT_MODE
  
 diff --git a/apps/aomdec.c b/apps/aomdec.c
-index 43aa5753dd..af4f2c2db4 100644
+index 43aa575..af4f2c2 100644
 --- a/apps/aomdec.c
 +++ b/apps/aomdec.c
 @@ -61,6 +61,26 @@ struct AvxDecInputContext {
@@ -142,7 +142,7 @@ index 43aa5753dd..af4f2c2db4 100644
                                      bru_opt_mode)) {
      fprintf(stderr, "Failed to set bru_opt_mode: %s\n",
 diff --git a/av1/av1_dx_iface.c b/av1/av1_dx_iface.c
-index be11305845..7d0ae14c4e 100644
+index be11305..7d0ae14 100644
 --- a/av1/av1_dx_iface.c
 +++ b/av1/av1_dx_iface.c
 @@ -51,6 +51,7 @@ struct aom_codec_alg_priv {
@@ -190,10 +190,10 @@ index be11305845..7d0ae14c4e 100644
    { AV1D_ENABLE_SUBGOP_STATS, ctrl_enable_subgop_stats },
  
 diff --git a/av1/decoder/decodeframe.c b/av1/decoder/decodeframe.c
-index 73b21114ec..529e7c1322 100644
+index e6f4c89..484503d 100644
 --- a/av1/decoder/decodeframe.c
 +++ b/av1/decoder/decodeframe.c
-@@ -10897,8 +10897,16 @@ void av1_decode_tg_tiles_and_wrapup(AV1Decoder *pbi, const uint8_t *data,
+@@ -10917,8 +10917,16 @@ void av1_decode_tg_tiles_and_wrapup(AV1Decoder *pbi, const uint8_t *data,
      }
    }
  
@@ -211,7 +211,7 @@ index 73b21114ec..529e7c1322 100644
        if (pbi->num_workers > 1) {
          av1_loop_filter_frame_mt(&cm->cur_frame->buf, cm, &pbi->dcb.xd, 0,
                                   num_planes, 0, pbi->tile_workers,
-@@ -10909,7 +10917,7 @@ void av1_decode_tg_tiles_and_wrapup(AV1Decoder *pbi, const uint8_t *data,
+@@ -10929,7 +10937,7 @@ void av1_decode_tg_tiles_and_wrapup(AV1Decoder *pbi, const uint8_t *data,
        }
      }
  
@@ -220,7 +220,7 @@ index 73b21114ec..529e7c1322 100644
          !pbi->skip_loop_filter && !cm->features.coded_lossless &&
          (cm->ccso_info.ccso_enable[0] || cm->ccso_info.ccso_enable[1] ||
           cm->ccso_info.ccso_enable[2]);
-@@ -10932,16 +10940,17 @@ void av1_decode_tg_tiles_and_wrapup(AV1Decoder *pbi, const uint8_t *data,
+@@ -10952,16 +10960,17 @@ void av1_decode_tg_tiles_and_wrapup(AV1Decoder *pbi, const uint8_t *data,
        extend_ccso_border(&cm->cur_frame->buf, ext_rec_y, CCSO_PADDING_SIZE);
      }
  
@@ -244,7 +244,7 @@ index 73b21114ec..529e7c1322 100644
  #if CONFIG_CONTROL_LOOPFILTERS_ACROSS_TILES
          !cm->seq_params.disable_loopfilters_across_tiles &&
 diff --git a/av1/decoder/decoder.h b/av1/decoder/decoder.h
-index 232423f86a..725b1f94ab 100644
+index 232423f..725b1f9 100644
 --- a/av1/decoder/decoder.h
 +++ b/av1/decoder/decoder.h
 @@ -322,6 +322,8 @@ typedef struct AV1Decoder {
@@ -257,5 +257,5 @@ index 232423f86a..725b1f94ab 100644
    int inv_tile_order;
    int need_resync;  // wait for key/intra-only frame.
 -- 
-2.48.1
+2.52.0
 

src/decode.c
@@ -278,7 +278,7 @@ static void derive_warpmv(const Dav1dTaskContext *const t,
         have_topright = off <= t->bx + bw4 && t->bx + bw4 < t->ts->tiling.col_end &&
             (!(t->by & (f->sb_step - 1)) || // top sb boundary
              ((t->bx + bw4) & (f->sb_step - 1) && // right sb boundary
-              t->is_coded[(t->by - 1) & 63] & (1ULL << ((t->bx + bw4) & 63))));
+              t->is_coded[0][(t->by - 1) & 63] & (1ULL << ((t->bx + bw4) & 63))));
     }
 
     if (np < 8 && have_left) {

src/internal.h
@@ -401,7 +401,7 @@ struct Dav1dTaskContext {
     int bx, by, cbx, cby, sdp_cfl_disallowed, intra_region;
     BlockContext l, *a;
     struct SBEdgeCtx a_sb_cache;
-    uint64_t is_coded[64];
+    uint64_t is_coded[2 /* luma, chroma */][64];
     struct {
         int col_end;
         int row_end;

src/ipred.h
@@ -36,8 +36,10 @@
 
 // These flags are OR'ed with the angle parameter in intra predictors.
 // They encode intra mode features such as edge availability, multi-line MRL,
-// DIP, IBP, and edge filtering.
+// DIP, IBP, edge filtering, and whether we're luma or chroma.
 //
+// ANGLE_IS_LUMA               - z1-3 use this flag to switch between a 4-tap
+//                               luma filter and a 2-tap (bilinear) chroma filter
 // ANGLE_DIP_FLAG              – enables directional intra prediction (DIP) mode
 // ANGLE_HAS_TOP_FLAG          – top reference edge is available for prediction
 // ANGLE_HAS_LEFT_FLAG         – left reference edge is available for prediction
@@ -47,6 +49,7 @@
 // ANGLE_USE_EDGE_FILTER_FLAG  – apply edge filtering (convolution) to reference samples
 // ANGLE_SMOOTH_TOP_EDGE_FLAG  – indicates smooth top edge; use reduced filter strength
 // ANGLE_SMOOTH_LEFT_EDGE_FLAG – indicates smooth left edge; use reduced filter strength
+#define ANGLE_IS_LUMA               (1 << 19)
 #define ANGLE_DIP_FLAG              (1 << 18)
 #define ANGLE_HAS_TOP_FLAG          (1 << 17)
 #define ANGLE_HAS_LEFT_FLAG         (1 << 16)

commit 2a8e771b713725be6a1999f76f79e0ce2165e4b8

Fix regression caused by bc1b93fc5 in luma z1.

src/ipred_tmpl.c
@@ -636,7 +636,7 @@ static void ipred_z1_c(pixel *dst, const ptrdiff_t stride,
         const int shift = (xpos & 0x3F) >> 1;
         const DRFilter4Tap f = av1_dr_interp_filter[shift];
         for (int x = 0; x < width; x++, base++) {
-            if (base >= max_base_x) {
+            if (base > max_base_x) {
                 pixel_set(&dst[y * PXSTRIDE(stride) + x], top[max_base_x],
                           width - x);
                 break;