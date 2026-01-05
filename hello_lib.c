81beb1c1e60ad9460f47448e9504dcc0e2770119
Martin Storsjö <martin@martin.st>
2026-01-06 00:17:27 +0200

    Try building a static library in arm64ec mode

--- hello_lib.c
+++ hello_lib.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
+void say_hello(void) {
     printf("Hello world!\n");
-    return 0;
 }
---
+++ hello_main.c
@@ -0,0 +1,6 @@
+void say_hello(void);
+
+int main(int argc, char* argv[]) {
+    say_hello();
+    return 0;
+}
