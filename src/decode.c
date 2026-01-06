4fbffd9cb310eae28db9ad3248818d7436babf9d
Ronald S. Bultje <rsbultje@gmail.com>
2026-01-06 16:17:14 -0500

    Fix have_topright statement in derive_warpmv().

--- src/decode.c
+++ src/decode.c
@@ -276,7 +276,7 @@ static void derive_warpmv(const Dav1dTaskContext *const t,
                 off += bs(&r2[off >> 1])[0];
             } while (off < w4 && np < 8);
         }
-        have_topright = off <= t->bx + bw4 && t->bx + bw4 < t->ts->tiling.col_end &&
+        have_topright = off <= bw4 && t->bx + bw4 < t->ts->tiling.col_end &&
             (!(t->by & (f->sb_step - 1)) || // top sb boundary
              ((t->bx + bw4) & (f->sb_step - 1) && // right sb boundary
               t->is_coded[0][(t->by - 1) & 63] & (1ULL << ((t->bx + bw4) & 63))));
