e0354cee981109f79f29b88a785c49c6a3556dc0
Martin Storsjö <martin@martin.st>
2026-02-10 11:30:39 +0200

    intops: Add an MSVC alternative to __builtin_popcount
    Use the __popcnt() intrinsic when we can assume that it is
    available; on arm64, and on x86/x64 if built with -arch:AVX.
    
    As a fallback, use a generic C implementation with a table.
    
    Use these for clang-cl as well. With __builtin_popcount(), Clang
    may either generate inline code, or a function call to a helper
    function, which is providedy by libgcc or compiler-rt; these are
    normally not available when building with clang-cl in an MSVC
    environment.

--- include/common/intops.h
+++ include/common/intops.h
@@ -86,7 +86,41 @@ static inline unsigned inv_recenter(const unsigned r, const unsigned v) {
 }
 
 static inline unsigned popcnt(const unsigned x) {
+#ifdef _MSC_VER
+#if defined(_M_ARM64) || defined(__AVX__)
+    // On x86/x64, this intrinsic produces a raw "popcnt" instruction, which
+    // only works if running on SSE4.2 or newer, and would require us to
+    // do runtime checking for the feature.
+    //
+    // Unconditionally use it, if we allow the compiler to use AVX anywhere
+    // (if built with /arch:AVX). Also use it on ARM64, where the corresponding
+    // instructions always are available.
+    return __popcnt(x);
+#else
+    static const uint8_t table[256] = {
+        0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+        4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
+    };
+    return table[x & 0xff]         + table[(x >> 8) & 0xff] +
+           table[(x >> 16) & 0xff] + table[(x >> 24) & 0xff];
+#endif
+#else
     return __builtin_popcount(x);
+#endif
 }
 
 #endif /* DAV2D_COMMON_INTOPS_H */
