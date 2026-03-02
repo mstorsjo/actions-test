8d88485d720a78161fa085ae063bd6f6421735af
Kyle Siefring <kylesiefring@twoorioles.com>
2026-03-02 10:36:36 -0500

    Remove old lr assembly

--- src/tables.h
+++ src/tables.h
@@ -82,8 +82,6 @@ EXTERN const int8_t dav2d_ns_wiener_coef_range_uv[18][2];
 EXTERN const uint8_t dav2d_pc_weiner_lut_to_class[4096];
 EXTERN const uint8_t dav2d_pc_wiener_sub_classify[4][256];
 EXTERN const uint8_t dav2d_pc_wiener_sub_classify_ns[4][7][256];
-EXTERN const uint16_t dav2d_sgr_params[16][2];
-EXTERN const uint8_t dav2d_sgr_x_by_x[256];
 
 EXTERN const int8_t dav2d_mc_subpel_filters[6][15][8];
 EXTERN const int8_t dav2d_ext_warp_filter[63][8];
