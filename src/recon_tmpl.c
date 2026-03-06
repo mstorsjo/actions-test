mstorsjo/actions-test

commit 3656f066568b5109868baaea1a9bd48f4e3bc842

Add a TxClass for inverse coefficient order
Used by identity transforms in some cases.

src/recon_tmpl.c
@@ -705,6 +705,7 @@ static int decode_coefs(Dav2dTaskContext *const t, DB_ONLY(const int depth)
         *txtp == IDTX && !chroma)
     {
         assert(!stx_type);
+        *txtp = IDTX_INV;
         int8_t *const levels = t->scratch.levels;
         const ptrdiff_t stride = 1 + (4 << slh);
         memset(levels, 0, stride * ((4 << slw) + 1));

src/levels.h
@@ -87,6 +87,7 @@ enum Tx1dType {
 
 enum TxClass {
     TX_CLASS_2D,
+    TX_CLASS_2D_INV, /* inverse coefficient order */
     TX_CLASS_H,
     TX_CLASS_V,
 };
@@ -106,6 +107,7 @@ enum TxfmType {
     TX_TYPE_ENUM_2D(FLIPADST, ADST),
     TX_TYPE_ENUM_2D(ADST, FLIPADST),
     TX_TYPE_ENUM(IDTX, IDENTITY, IDENTITY, 2D),
+    TX_TYPE_ENUM(IDTX_INV, IDENTITY, IDENTITY, 2D_INV),
     TX_TYPE_ENUM(V_DCT, IDENTITY, DCT, V),
     TX_TYPE_ENUM(H_DCT, DCT, IDENTITY, H),
     TX_TYPE_ENUM(V_ADST, IDENTITY, ADST, V),

commit cd2789cded491153c304e79cfcdb19da58256c45

decode_coefs: don't use IDTX_INV for chroma.
This fixes crashes introduced by 3656f066 when inter blocks use
IDTX (which turns into IDTX_INV) and chroma blocks infer the txtp.
For example, when decoding enable-tx-partition0.obu from #26:

Assertion failed: (0), function decode_coefs, file recon_tmpl.c, line 998.
Process 64497 stopped
* thread #1, queue = 'com.apple.main-thread', stop reason = hit program assert
    frame #4: 0x00000001009bcdc3 libdav2d.1.dylib`decode_coefs(t=0x000000010071e800, depth=11, a="..", l="..", tx=0, bs=BS_256x256, b=0x00007ff7bfefbea0, plane=1, cf=0x00000001007a02c0, txtp=0x000000010079fa80, res_ctx="\U00000001") at recon_tmpl.c:998:18 [opt]
   995 	            DECODE_COEFS_CLASS(TX_CLASS_V, y, 0);
   996 	        }
   997 	#undef DECODE_COEFS_CLASS
-> 998 	        default: assert(0);
   999 	        }
   1000	    } else if (chroma) { // dc-only
   1001	        dc_tok = 1 + dav2d_msac_decode_symbol_adapt8(&ts->msac,

src/recon_tmpl.c
@@ -436,7 +436,7 @@ static int decode_coefs(Dav2dTaskContext *const t, DB_ONLY(const int depth)
               (*txtp & 0xe2) == 0x22 /* identity ver, (flip)adst hor */)))
         {
             *txtp = DCT_DCT;
-        }
+        } else if (*txtp == IDTX_INV) *txtp = IDTX;
     } else if (intra) {
         if (t_dim->sub == TX_32X32 /* 64x64, 64x32 or 32x64 */) {
             *txtp = DCT_DCT;