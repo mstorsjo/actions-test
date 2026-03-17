videolan/dav1d

Improve the memory pool API
Return a void pointer directly to the usable memory region,
abstracting away implementation details.

src/mem.c
@@ -221,8 +221,9 @@ static COLD void mem_pool_destroy(Dav1dMemPool *const pool) {
     dav1d_free(pool);
 }
 
-void dav1d_mem_pool_push(Dav1dMemPool *const pool, Dav1dMemPoolBuffer *const buf) {
+void dav1d_mem_pool_push(Dav1dMemPool *const pool, void *const ptr) {
     pthread_mutex_lock(&pool->lock);
+    Dav1dMemPoolBuffer *const buf = (Dav1dMemPoolBuffer*)((uintptr_t)ptr - 64);
     const int ref_cnt = --pool->ref_cnt;
     if (!pool->end) {
         buf->next = pool->buf;
@@ -231,24 +232,22 @@ void dav1d_mem_pool_push(Dav1dMemPool *const pool, Dav1dMemPoolBuffer *const buf
         assert(ref_cnt > 0);
     } else {
         pthread_mutex_unlock(&pool->lock);
-        dav1d_free_aligned(buf->data);
+        dav1d_free_aligned(buf);
         if (!ref_cnt) mem_pool_destroy(pool);
     }
 }
 
-Dav1dMemPoolBuffer *dav1d_mem_pool_pop(Dav1dMemPool *const pool, const size_t size) {
-    assert(!(size & (sizeof(void*) - 1)));
+void *dav1d_mem_pool_pop(Dav1dMemPool *const pool, const size_t size) {
     pthread_mutex_lock(&pool->lock);
     Dav1dMemPoolBuffer *buf = pool->buf;
     pool->ref_cnt++;
-    uint8_t *data;
+
     if (buf) {
         pool->buf = buf->next;
         pthread_mutex_unlock(&pool->lock);
-        data = buf->data;
-        if ((uintptr_t)buf - (uintptr_t)data != size) {
+        if (buf->size != size) {
             /* Reallocate if the size has changed */
-            dav1d_free_aligned(data);
+            dav1d_free_aligned(buf);
             goto alloc;
         }
 #if TRACK_HEAP_ALLOCATIONS
@@ -257,20 +256,18 @@ Dav1dMemPoolBuffer *dav1d_mem_pool_pop(Dav1dMemPool *const pool, const size_t si
     } else {
         pthread_mutex_unlock(&pool->lock);
 alloc:
-        data = dav1d_alloc_aligned(pool->type,
-                                   size + sizeof(Dav1dMemPoolBuffer), 64);
-        if (!data) {
+        buf = dav1d_alloc_aligned(pool->type, size + 64, 64);
+        if (!buf) {
             pthread_mutex_lock(&pool->lock);
             const int ref_cnt = --pool->ref_cnt;
             pthread_mutex_unlock(&pool->lock);
             if (!ref_cnt) mem_pool_destroy(pool);
             return NULL;
         }
-        buf = (Dav1dMemPoolBuffer*)(data + size);
-        buf->data = data;
+        buf->size = size;
     }
 
-    return buf;
+    return (void*)((uintptr_t)buf + 64);
 }
 
 COLD int dav1d_mem_pool_init(const enum AllocationType type,
@@ -305,9 +302,9 @@ COLD void dav1d_mem_pool_end(Dav1dMemPool *const pool) {
         pthread_mutex_unlock(&pool->lock);
 
         while (buf) {
-            void *const data = buf->data;
+            void *const ptr = buf;
             buf = buf->next;
-            dav1d_free_aligned(data);
+            dav1d_free_aligned(ptr);
         }
         if (!ref_cnt) mem_pool_destroy(pool);
     }

src/mem.h
@@ -65,8 +65,8 @@ enum AllocationType {
 };
 
 typedef struct Dav1dMemPoolBuffer {
-    void *data;
     struct Dav1dMemPoolBuffer *next;
+    size_t size;
 } Dav1dMemPoolBuffer;
 
 typedef struct Dav1dMemPool {
@@ -136,8 +136,8 @@ void dav1d_log_alloc_stats(Dav1dContext *c);
 #define dav1d_free_aligned(ptr) dav1d_free_aligned_internal(ptr)
 #endif /* TRACK_HEAP_ALLOCATIONS */
 
-void dav1d_mem_pool_push(Dav1dMemPool *pool, Dav1dMemPoolBuffer *buf);
-Dav1dMemPoolBuffer *dav1d_mem_pool_pop(Dav1dMemPool *pool, size_t size);
+void dav1d_mem_pool_push(Dav1dMemPool *pool, void *ptr);
+void *dav1d_mem_pool_pop(Dav1dMemPool *pool, size_t size);
 int dav1d_mem_pool_init(enum AllocationType type, Dav1dMemPool **pool);
 void dav1d_mem_pool_end(Dav1dMemPool *pool);
 

src/picture.c
@@ -44,7 +44,6 @@
 #include "src/thread_task.h"
 
 int dav1d_default_picture_alloc(Dav1dPicture *const p, void *const cookie) {
-    assert(sizeof(Dav1dMemPoolBuffer) <= DAV1D_PICTURE_ALIGNMENT);
     const int hbd = p->p.bpc > 8;
     const int aligned_w = (p->p.w + 127) & ~127;
     const int aligned_h = (p->p.h + 127) & ~127;
@@ -68,16 +67,12 @@ int dav1d_default_picture_alloc(Dav1dPicture *const p, void *const cookie) {
     const size_t uv_sz = uv_stride * (aligned_h >> ss_ver);
     const size_t pic_size = y_sz + 2 * uv_sz;
 
-    Dav1dMemPoolBuffer *const buf = dav1d_mem_pool_pop(cookie, pic_size +
-                                                       DAV1D_PICTURE_ALIGNMENT -
-                                                       sizeof(Dav1dMemPoolBuffer));
+    uint8_t *const buf = dav1d_mem_pool_pop(cookie, pic_size + DAV1D_PICTURE_ALIGNMENT);
     if (!buf) return DAV1D_ERR(ENOMEM);
     p->allocator_data = buf;
-
-    uint8_t *const data = buf->data;
-    p->data[0] = data;
-    p->data[1] = has_chroma ? data + y_sz : NULL;
-    p->data[2] = has_chroma ? data + y_sz + uv_sz : NULL;
+    p->data[0] = buf;
+    p->data[1] = has_chroma ? buf + y_sz : NULL;
+    p->data[2] = has_chroma ? buf + y_sz + uv_sz : NULL;
 
     return 0;
 }
@@ -94,12 +89,11 @@ struct pic_ctx_context {
 };
 
 static void free_buffer(const uint8_t *const data, void *const user_data) {
-    Dav1dMemPoolBuffer *buf = (Dav1dMemPoolBuffer *)data;
-    struct pic_ctx_context *pic_ctx = buf->data;
+    struct pic_ctx_context *pic_ctx = (struct pic_ctx_context*)data;
 
     pic_ctx->allocator.release_picture_callback(&pic_ctx->pic,
                                                 pic_ctx->allocator.cookie);
-    dav1d_mem_pool_push(user_data, buf);
+    dav1d_mem_pool_push(user_data, pic_ctx);
 }
 
 void dav1d_picture_free_itut_t35(const uint8_t *const data, void *const user_data) {
@@ -128,12 +122,10 @@ static int picture_alloc(Dav1dContext *const c,
     assert(bpc > 0 && bpc <= 16);
 
     size_t extra = c->n_fc > 1 ? sizeof(atomic_int) * 2 : 0;
-    Dav1dMemPoolBuffer *buf = dav1d_mem_pool_pop(c->pic_ctx_pool,
-                                                 extra + sizeof(struct pic_ctx_context));
-    if (buf == NULL)
+    struct pic_ctx_context *pic_ctx = dav1d_mem_pool_pop(c->pic_ctx_pool, extra +
+                                                         sizeof(struct pic_ctx_context));
+    if (!pic_ctx)
         return DAV1D_ERR(ENOMEM);
-
-    struct pic_ctx_context *pic_ctx = buf->data;
 
     p->p.w = w;
     p->p.h = h;
@@ -144,13 +136,13 @@ static int picture_alloc(Dav1dContext *const c,
     dav1d_data_props_set_defaults(&p->m);
     const int res = p_allocator->alloc_picture_callback(p, p_allocator->cookie);
     if (res < 0) {
-        dav1d_mem_pool_push(c->pic_ctx_pool, buf);
+        dav1d_mem_pool_push(c->pic_ctx_pool, pic_ctx);
         return res;
     }
 
     pic_ctx->allocator = *p_allocator;
     pic_ctx->pic = *p;
-    p->ref = dav1d_ref_init(&pic_ctx->ref, buf, free_buffer, c->pic_ctx_pool, 0);
+    p->ref = dav1d_ref_init(&pic_ctx->ref, pic_ctx, free_buffer, c->pic_ctx_pool, 0);
 
     p->seq_hdr_ref = seq_hdr_ref;
     if (seq_hdr_ref) dav1d_ref_inc(seq_hdr_ref);
@@ -237,8 +229,7 @@ int dav1d_thread_picture_alloc(Dav1dContext *const c, Dav1dFrameContext *const f
 int dav1d_picture_alloc_copy(Dav1dContext *const c, Dav1dPicture *const dst, const int w,
                              const Dav1dPicture *const src)
 {
-    Dav1dMemPoolBuffer *const buf = (Dav1dMemPoolBuffer *)src->ref->const_data;
-    struct pic_ctx_context *const pic_ctx = buf->data;
+    struct pic_ctx_context *const pic_ctx = (struct pic_ctx_context*)src->ref->const_data;
     const int res = picture_alloc(c, dst, w, src->p.h,
                                   src->seq_hdr, src->seq_hdr_ref,
                                   src->frame_hdr, src->frame_hdr_ref,

src/ref.c
@@ -54,14 +54,13 @@ static void pool_free_callback(const uint8_t *const data, void *const user_data)
 }
 
 Dav1dRef *dav1d_ref_create_using_pool(Dav1dMemPool *const pool, size_t size) {
-    size = (size + sizeof(void*) - 1) & ~(sizeof(void*) - 1);
-
-    Dav1dMemPoolBuffer *const buf =
-        dav1d_mem_pool_pop(pool, size + sizeof(Dav1dRef));
+    void *const buf = dav1d_mem_pool_pop(pool, size);
     if (!buf) return NULL;
 
+    /* Store Dav1dRef inside the Dav1dMemPoolBuffer alignment padding */
+    assert(sizeof(Dav1dMemPoolBuffer) + sizeof(Dav1dRef) <= 64);
     Dav1dRef *const res = &((Dav1dRef*)buf)[-1];
-    res->data = buf->data;
+    res->data = buf;
     res->const_data = pool;
     atomic_init(&res->ref_cnt, 1);
     res->free_ref = 0;

mstorsjo/actions-test

src/mem.c
@@ -222,8 +222,9 @@ static COLD void mem_pool_destroy(Dav2dMemPool *const pool) {
     dav2d_free(pool);
 }
 
-void dav2d_mem_pool_push(Dav2dMemPool *const pool, Dav2dMemPoolBuffer *const buf) {
+void dav2d_mem_pool_push(Dav2dMemPool *const pool, void *const ptr) {
     pthread_mutex_lock(&pool->lock);
+    Dav2dMemPoolBuffer *const buf = (Dav2dMemPoolBuffer*)((uintptr_t)ptr - 64);
     const int ref_cnt = --pool->ref_cnt;
     if (!pool->end) {
         buf->next = pool->buf;
@@ -232,24 +233,22 @@ void dav2d_mem_pool_push(Dav2dMemPool *const pool, Dav2dMemPoolBuffer *const buf
         assert(ref_cnt > 0);
     } else {
         pthread_mutex_unlock(&pool->lock);
-        dav2d_free_aligned(buf->data);
+        dav2d_free_aligned(buf);
         if (!ref_cnt) mem_pool_destroy(pool);
     }
 }
 
-Dav2dMemPoolBuffer *dav2d_mem_pool_pop(Dav2dMemPool *const pool, const size_t size) {
-    assert(!(size & (sizeof(void*) - 1)));
+void *dav2d_mem_pool_pop(Dav2dMemPool *const pool, const size_t size) {
     pthread_mutex_lock(&pool->lock);
     Dav2dMemPoolBuffer *buf = pool->buf;
     pool->ref_cnt++;
-    uint8_t *data;
+
     if (buf) {
         pool->buf = buf->next;
         pthread_mutex_unlock(&pool->lock);
-        data = buf->data;
-        if ((uintptr_t)buf - (uintptr_t)data != size) {
+        if (buf->size != size) {
             /* Reallocate if the size has changed */
-            dav2d_free_aligned(data);
+            dav2d_free_aligned(buf);
             goto alloc;
         }
 #if TRACK_HEAP_ALLOCATIONS
@@ -258,20 +257,18 @@ Dav2dMemPoolBuffer *dav2d_mem_pool_pop(Dav2dMemPool *const pool, const size_t si
     } else {
         pthread_mutex_unlock(&pool->lock);
 alloc:
-        data = dav2d_alloc_aligned(pool->type,
-                                   size + sizeof(Dav2dMemPoolBuffer), 64);
-        if (!data) {
+        buf = dav2d_alloc_aligned(pool->type, size + 64, 64);
+        if (!buf) {
             pthread_mutex_lock(&pool->lock);
             const int ref_cnt = --pool->ref_cnt;
             pthread_mutex_unlock(&pool->lock);
             if (!ref_cnt) mem_pool_destroy(pool);
             return NULL;
         }
-        buf = (Dav2dMemPoolBuffer*)(data + size);
-        buf->data = data;
+        buf->size = size;
     }
 
-    return buf;
+    return (void*)((uintptr_t)buf + 64);
 }
 
 COLD int dav2d_mem_pool_init(const enum AllocationType type,
@@ -306,9 +303,9 @@ COLD void dav2d_mem_pool_end(Dav2dMemPool *const pool) {
         pthread_mutex_unlock(&pool->lock);
 
         while (buf) {
-            void *const data = buf->data;
+            void *const ptr = buf;
             buf = buf->next;
-            dav2d_free_aligned(data);
+            dav2d_free_aligned(ptr);
         }
         if (!ref_cnt) mem_pool_destroy(pool);
     }

src/mem.h
@@ -66,8 +66,8 @@ enum AllocationType {
 };
 
 typedef struct Dav2dMemPoolBuffer {
-    void *data;
     struct Dav2dMemPoolBuffer *next;
+    size_t size;
 } Dav2dMemPoolBuffer;
 
 typedef struct Dav2dMemPool {
@@ -137,8 +137,8 @@ void dav2d_log_alloc_stats(Dav2dContext *c);
 #define dav2d_free_aligned(ptr) dav2d_free_aligned_internal(ptr)
 #endif /* TRACK_HEAP_ALLOCATIONS */
 
-void dav2d_mem_pool_push(Dav2dMemPool *pool, Dav2dMemPoolBuffer *buf);
-Dav2dMemPoolBuffer *dav2d_mem_pool_pop(Dav2dMemPool *pool, size_t size);
+void dav2d_mem_pool_push(Dav2dMemPool *pool, void *ptr);
+void *dav2d_mem_pool_pop(Dav2dMemPool *pool, size_t size);
 int dav2d_mem_pool_init(enum AllocationType type, Dav2dMemPool **pool);
 void dav2d_mem_pool_end(Dav2dMemPool *pool);
 

src/picture.c
@@ -44,7 +44,6 @@
 #include "src/thread_task.h"
 
 int dav2d_default_picture_alloc(Dav2dPicture *const p, void *const cookie) {
-    assert(sizeof(Dav2dMemPoolBuffer) <= DAV2D_PICTURE_ALIGNMENT);
     const int hbd = p->p.bpc > 8;
     const int aligned_w = (p->p.w + 127) & ~127;
     const int aligned_h = (p->p.h + 127) & ~127;
@@ -68,16 +67,12 @@ int dav2d_default_picture_alloc(Dav2dPicture *const p, void *const cookie) {
     const size_t uv_sz = uv_stride * (aligned_h >> ss_ver);
     const size_t pic_size = y_sz + 2 * uv_sz;
 
-    Dav2dMemPoolBuffer *const buf = dav2d_mem_pool_pop(cookie, pic_size +
-                                                       DAV2D_PICTURE_ALIGNMENT -
-                                                       sizeof(Dav2dMemPoolBuffer));
+    uint8_t *const buf = dav2d_mem_pool_pop(cookie, pic_size + DAV2D_PICTURE_ALIGNMENT);
     if (!buf) return DAV2D_ERR(ENOMEM);
     p->allocator_data = buf;
-
-    uint8_t *const data = buf->data;
-    p->data[0] = data;
-    p->data[1] = has_chroma ? data + y_sz : NULL;
-    p->data[2] = has_chroma ? data + y_sz + uv_sz : NULL;
+    p->data[0] = buf;
+    p->data[1] = has_chroma ? buf + y_sz : NULL;
+    p->data[2] = has_chroma ? buf + y_sz + uv_sz : NULL;
 
     return 0;
 }
@@ -94,12 +89,11 @@ struct pic_ctx_context {
 };
 
 static void free_buffer(const uint8_t *const data, void *const user_data) {
-    Dav2dMemPoolBuffer *buf = (Dav2dMemPoolBuffer *)data;
-    struct pic_ctx_context *pic_ctx = buf->data;
+    struct pic_ctx_context *pic_ctx = (struct pic_ctx_context*)data;
 
     pic_ctx->allocator.release_picture_callback(&pic_ctx->pic,
                                                 pic_ctx->allocator.cookie);
-    dav2d_mem_pool_push(user_data, buf);
+    dav2d_mem_pool_push(user_data, pic_ctx);
 }
 
 void dav2d_picture_free_itut_t35(const uint8_t *const data, void *const user_data) {
@@ -128,12 +122,10 @@ static int picture_alloc(Dav2dContext *const c,
     assert(bpc > 0 && bpc <= 16);
 
     size_t extra = c->n_fc > 1 ? sizeof(atomic_int) * 2 : 0;
-    Dav2dMemPoolBuffer *buf = dav2d_mem_pool_pop(c->pic_ctx_pool,
-                                                 extra + sizeof(struct pic_ctx_context));
-    if (buf == NULL)
+    struct pic_ctx_context *pic_ctx = dav2d_mem_pool_pop(c->pic_ctx_pool, extra +
+                                                         sizeof(struct pic_ctx_context));
+    if (!pic_ctx)
         return DAV2D_ERR(ENOMEM);
-
-    struct pic_ctx_context *pic_ctx = buf->data;
 
     p->p.w = w;
     p->p.h = h;
@@ -144,13 +136,13 @@ static int picture_alloc(Dav2dContext *const c,
     dav2d_data_props_set_defaults(&p->m);
     const int res = p_allocator->alloc_picture_callback(p, p_allocator->cookie);
     if (res < 0) {
-        dav2d_mem_pool_push(c->pic_ctx_pool, buf);
+        dav2d_mem_pool_push(c->pic_ctx_pool, pic_ctx);
         return res;
     }
 
     pic_ctx->allocator = *p_allocator;
     pic_ctx->pic = *p;
-    p->ref = dav2d_ref_init(&pic_ctx->ref, buf, free_buffer, c->pic_ctx_pool, 0);
+    p->ref = dav2d_ref_init(&pic_ctx->ref, pic_ctx, free_buffer, c->pic_ctx_pool, 0);
 
     p->seq_hdr_ref = seq_hdr_ref;
     if (seq_hdr_ref) dav2d_ref_inc(seq_hdr_ref);
@@ -239,8 +231,7 @@ int dav2d_thread_picture_alloc(Dav2dContext *const c, Dav2dFrameContext *const f
 int dav2d_picture_alloc_copy(Dav2dContext *const c, Dav2dPicture *const dst,
                              const Dav2dPicture *const src)
 {
-    Dav2dMemPoolBuffer *const buf = (Dav2dMemPoolBuffer *)src->ref->const_data;
-    struct pic_ctx_context *const pic_ctx = buf->data;
+    struct pic_ctx_context *const pic_ctx = (struct pic_ctx_context*)src->ref->const_data;
     const int res = picture_alloc(c, dst, src->p.w, src->p.h,
                                   src->seq_hdr, src->seq_hdr_ref,
                                   src->frame_hdr, src->frame_hdr_ref,

src/ref.c
@@ -54,14 +54,13 @@ static void pool_free_callback(const uint8_t *const data, void *const user_data)
 }
 
 Dav2dRef *dav2d_ref_create_using_pool(Dav2dMemPool *const pool, size_t size) {
-    size = (size + sizeof(void*) - 1) & ~(sizeof(void*) - 1);
-
-    Dav2dMemPoolBuffer *const buf =
-        dav2d_mem_pool_pop(pool, size + sizeof(Dav2dRef));
+    void *const buf = dav2d_mem_pool_pop(pool, size);
     if (!buf) return NULL;
 
+    /* Store Dav2dRef inside the Dav2dMemPoolBuffer alignment padding */
+    assert(sizeof(Dav2dMemPoolBuffer) + sizeof(Dav2dRef) <= 64);
     Dav2dRef *const res = &((Dav2dRef*)buf)[-1];
-    res->data = buf->data;
+    res->data = buf;
     res->const_data = pool;
     atomic_init(&res->ref_cnt, 1);
     res->free_ref = 0;