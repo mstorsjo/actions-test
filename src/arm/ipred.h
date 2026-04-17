d8b3339a69fb784f70280d7de8d07c7e4d38c3e8
Arpad Panyik <Arpad.Panyik@arm.com>
2026-04-17 11:41:25 +0200

    AArch64: Add Neon implementation of ipred_smooth_v_8bpc
    Introduce a Neon implementation for ipred_smooth_v_8bpc.
    
    The implementation handles all block sizes without a jump table by
    splitting the code into only three specialized paths:
     - width = 4: processes 4 rows per vector for higher performance
     - width = 8: processes 2 rows per vector for improved throughput
     - width >= 16: handles larger block sizes
    
    Only fixed vector shifts are used. Narrowing right shifts are
    implemented with ADDHN[2] to improve throughput on most CPUs. All
    handlers contain specialised path for zero weights to increase
    throughput.
    
    Relative uplifts of intra_pred_smooth_v_*_8bpc_neon on some Cortex
    CPUs relative to the C reference compiled with Clang-22:
    
     *    A55   A510   A520   A76   A78  A715  A725    X1    X3  X925
    w4:  6.85x  4.59x  6.46x 4.24x 3.63x 4.92x 4.55x 3.75x 3.37x 4.24x
    w8:  9.96x 11.66x 11.20x 5.80x 5.22x 6.11x 6.05x 5.53x 5.17x 5.45x
    w16: 5.57x  7.04x  6.69x 5.17x 4.68x 4.41x 4.42x 4.88x 4.55x 4.68x
    w32: 5.01x  6.34x  6.20x 4.77x 4.48x 4.14x 4.14x 4.65x 4.30x 4.29x
    w64: 4.52x  5.78x  5.76x 4.45x 4.13x 3.83x 3.82x 4.32x 4.03x 3.97x

--- src/arm/ipred.h
+++ src/arm/ipred.h
@@ -296,10 +296,10 @@ static ALWAYS_INLINE void intra_pred_dsp_init_arm(Dav2dIntraPredDSPContext *cons
     c->intra_pred[VERT_PRED]     = BF(dav2d_ipred_v, neon);
     c->intra_pred[PAETH_PRED]    = BF(dav2d_ipred_paeth, neon);
     c->intra_pred[SMOOTH_PRED]   = BF(dav2d_ipred_smooth, neon);
-    c->intra_pred[SMOOTH_V_PRED] = BF(dav2d_ipred_smooth_v, neon);
 #endif
 #if ARCH_AARCH64 && BITDEPTH == 8
     c->intra_pred[SMOOTH_H_PRED] = BF(dav2d_ipred_smooth_h, neon);
+    c->intra_pred[SMOOTH_V_PRED] = BF(dav2d_ipred_smooth_v, neon);
 #endif
 #if ARCH_AARCH64 && 0
     c->intra_pred[Z1_PRED]       = ipred_z1_neon;
