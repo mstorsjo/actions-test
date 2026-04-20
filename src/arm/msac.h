d652f4991ff14f9c1266df23183665e85846918e
Martin Storsjö <martin@martin.st>
2026-04-20 14:20:18 +0300

    arm: msac: Stop using old msac assembly functions
    These aren't yet up to date for AV2.
    
    If trying to do decoding on arm/aarch64, then we hit failed
    asserts and/or crash.

--- src/arm/msac.h
+++ src/arm/msac.h
@@ -38,12 +38,6 @@ unsigned dav2d_msac_decode_bool_equi_neon(MsacContext *s);
 unsigned dav2d_msac_decode_bool_neon(MsacContext *s, unsigned f);
 
 #if defined(__ARM_NEON) || defined(__APPLE__) || defined(_WIN32) || ARCH_AARCH64
-#define dav2d_msac_decode_symbol_adapt4  dav2d_msac_decode_symbol_adapt4_neon
-#define dav2d_msac_decode_symbol_adapt8  dav2d_msac_decode_symbol_adapt8_neon
-#define dav2d_msac_decode_hi_tok         dav2d_msac_decode_hi_tok_neon
-#define dav2d_msac_decode_bool_adapt     dav2d_msac_decode_bool_adapt_neon
-#define dav2d_msac_decode_bool_equi      dav2d_msac_decode_bool_equi_neon
-#define dav2d_msac_decode_bool           dav2d_msac_decode_bool_neon
 #endif
 
 #endif /* DAV2D_SRC_ARM_MSAC_H */
